8e2839c201cdc864d202ea882c290d76df81b517
Dylan Nelson <3587498+dnelson86@users.noreply.github.com>
2016-01-04 08:03:00 -0500

    Removed unused misc.

--- Mark/PeanoHilbertKey/src/proto.h
+++
@@ -1,9 +0,0 @@
-#include <malloc.h>
-
-void get_keys(void);
-void get_pos(void);
-void endrun(int ierr);
-void free_particle_data(void);
-
-#define min(x,y) ((x) < (y) ? (x) : (y))
-#define max(x,y) ((x) > (y) ? (x) : (y))
--- Mark/PeanoHilbertKey/src/setup.py
+++
@@ -1,7 +0,0 @@
-from distutils.core import setup, Extension
-
-setup(name = "PeanoHilbertKey", version = "0.1",
-  ext_modules = [
-    Extension("PeanoHilbertKey", ["main.c","peano.c","allvars.c"])
-    ]
-)
