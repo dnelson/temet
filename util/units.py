8b3c0f3d698dc343cb925da6100609ab15e2d26b
Dylan Nelson <3587498+dnelson86@users.noreply.github.com>
2016-05-02 07:21:03 -0400

    Minor bugfix util.units.codeMassToLogMsun()

--- util/units.py
+++ util/units.py
@@ -122,7 +122,7 @@ class units(object):
 
     def codeMassToLogMsun(self, mass):
         """ Convert mass from code units (10**10 msun/h) to (log msun). """
-        return logZeroSafe( codeMassToMsun(mass) )
+        return logZeroSafe( self.codeMassToMsun(mass) )
 
     def codeMassToVirTemp(self, mass, meanmolwt=None, log=False):
         """ Convert from halo mass in code units to virial temperature in Kelvin, 
