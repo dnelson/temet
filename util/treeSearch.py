a958dddb33cd1ed77893ca3930f75ea3f15ab2e0
Dylan Nelson <dnelson@cfa.harvard.edu>
2016-06-15 13:05:35 -0400

    util/treeSearch.py serial JITed neighbor search functional.

--- util/treeSearch.py
+++ util/treeSearch.py
@@ -7,14 +7,17 @@ from builtins import *
 
 import numpy as np
 import threading
+import time
+
 from numba import jit, void, int32
 from util.helper import pSplit
+from util.sphMap import _NEAREST, _getkernel
 
 #@jit(void(int32,int32,int32,int32,int32[:],int32[:],int32[:],int32[:]))
 @jit(nopython=True, nogil=True)
 def _updateNodeRecursive(no,sib,NumPart,last,suns,nextnode,next_node,sibling):
     """ Helper routine for calcHsml(), see below. """
-    #print(' _updateNodeRecursive(%d,%d)' % (no,sib))
+    #print(' _updateNodeRecursive(%d,%d,%d,%d)' % (no,sib,NumPart,last))
     pp = 0 #np.int32(0)
     nextsib = 0 #np.int32(0)
 
@@ -32,22 +35,19 @@ def _updateNodeRecursive(no,sib,NumPart,last,suns,nextnode,next_node,sibling):
 
             if p >= 0:
                 # check if we have a sibling on the same level
-                for j in range(i+1,8):
+                j = i + 1
+                while j < 8:
                     pp = suns[j,no-NumPart]
                     if pp >= 0:
                         break
+                    j += 1 # unusual syntax so that j==8 at the end of the loop if we never break
 
-                    if j < 8: # yes, we do
-                        nextsib = pp
-                    else:
-                        nextsib = sib
+                if j < 8: # yes, we do
+                    nextsib = pp
+                else:
+                    nextsib = sib
 
-                    #p = np.int32(p)
-                    #nextsib = np.int32(nextsib)
-                    #NumPart = np.int32(NumPart)
-                    #last = np.int32(last)
-
-                    _updateNodeRecursive(p,nextsib,NumPart,last,suns,nextnode,next_node,sibling)
+                last = _updateNodeRecursive(p,nextsib,NumPart,last,suns,nextnode,next_node,sibling)
 
         sibling[no-NumPart] = sib
 
@@ -60,6 +60,8 @@ def _updateNodeRecursive(no,sib,NumPart,last,suns,nextnode,next_node,sibling):
                 next_node[last] = no
 
         last = no
+
+    return last # avoid use of global in numba
 
 @jit(nopython=True, nogil=True)
 def _constructTree(pos,boxSizeSim,next_node, #father_node,
@@ -81,7 +83,6 @@ def _constructTree(pos,boxSizeSim,next_node, #father_node,
     # select first node
     NumPart = pos.shape[0]
     nFree = NumPart
-    #nfreep = Nodes[nFree]
 
     # create an empty root node
     if boxSizeSim > 0.0:
@@ -129,92 +130,81 @@ def _constructTree(pos,boxSizeSim,next_node, #father_node,
             if no >= NumPart: # we are dealing with an internal node
                 # to which subnode will this particle belong
                 subnode = 0
+                ind = no-NumPart
 
-                if pos[i,0] > center[0,no-NumPart]:
+                if pos[i,0] > center[0,ind]:
                     subnode += 1
-                if pos[i,1] > center[1,no-NumPart]:
+                if pos[i,1] > center[1,ind]:
                     subnode += 2
-                if pos[i,2] > center[2,no-NumPart]:
+                if pos[i,2] > center[2,ind]:
                     subnode += 4
 
-                #print('p[%d] subnode = %d (no=%d no-NumPart=%d)' % (i,subnode,no,no-NumPart))
-
                 # get the next node
-                nn = suns[subnode,no-NumPart]
+                nn = suns[subnode,ind]
 
                 if nn >= 0: # ok, something is in the daughter slot already, need to continue
                     parent = no # note: subnode can still be used in the next step of the walk
                     no = nn
                 else:
                     # here we have found an empty slot where we can attach the new particle as a leaf
-                    suns[subnode,no-NumPart] = i
-                    #print(' attached as leaf suns[%d,%d]' % (subnode,no-NumPart))
+                    suns[subnode,ind] = i
                     break # done for this particle
             else:
                 # we try to insert into a leaf with a single particle - need to generate a new internal
                 # node at this point, because every leaf is only allowed to contain one particle
                 suns[subnode,parent-NumPart] = nFree
+                ind1 = parent-NumPart
+                ind2 = nFree-NumPart
 
-                length[nFree-NumPart] = 0.5 * length[parent-NumPart]
-                lenHalf = 0.25 * length[parent-NumPart]
+                length[ind2] = 0.5 * length[ind1]
+                lenHalf = 0.25 * length[ind1]
 
                 if subnode & 1: # np.bitwise_and(subnode,1)
-                    center[0,nFree-NumPart] = center[0,parent-NumPart] + lenHalf
+                    center[0,ind2] = center[0,ind1] + lenHalf
                 else:
-                    center[0,nFree-NumPart] = center[0,parent-NumPart] - lenHalf
+                    center[0,ind2] = center[0,ind1] - lenHalf
 
                 if subnode & 2:
-                    center[1,nFree-NumPart] = center[1,parent-NumPart] + lenHalf
+                    center[1,ind2] = center[1,ind1] + lenHalf
                 else:
-                    center[1,nFree-NumPart] = center[1,parent-NumPart] - lenHalf
+                    center[1,ind2] = center[1,ind1] - lenHalf
 
                 if subnode & 4:
-                    center[2,nFree-NumPart] = center[2,parent-NumPart] + lenHalf
+                    center[2,ind2] = center[2,ind1] + lenHalf
                 else:
-                    center[2,nFree-NumPart] = center[2,parent-NumPart] - lenHalf
+                    center[2,ind2] = center[2,ind1] - lenHalf
 
                 for j in range(8):
-                    suns[j,nFree-NumPart] = -1
+                    suns[j,ind2] = -1
 
                 # which subnode
                 subnode = 0
 
-                if pos[no-NumPart,0] > center[0,nFree-NumPart]:
+                if pos[no,0] > center[0,ind2]:
                     subnode += 1
-                if pos[no-NumPart,1] > center[1,nFree-NumPart]:
+                if pos[no,1] > center[1,ind2]:
                     subnode += 2
-                if pos[no-NumPart,2] > center[2,nFree-NumPart]:
+                if pos[no,2] > center[2,ind2]:
                     subnode += 4
 
                 #if length[nFree] < 1.0e-3 * Softening:
                     # randomize subnode index, or terminate
 
-                suns[subnode,nFree-NumPart] = no
+                suns[subnode,ind2] = no
 
                 no = nFree # resume trying to insert the new particle at the newly created internal node
 
                 numNodes += 1
                 nFree += 1
 
-                if numNodes >= length.shape[0]:
-                    return 0 # e.g. request reallocation and retry
+                assert numNodes < length.shape[0] # Exceed tree allocated size, need to increase and redo.
 
     # now compute the multipole moments recursively
-    #last = np.zeros( 1, dtype=np.int32) -1
-    #dummy = np.zeros( 1, dtype=np.int32) -1
-
     last = np.int32(-1)
-    dummy = np.int32(-1)
-    #NumPart = np.int32(NumPart)
+    #dummy = np.int32(-1)
 
-    #print(last.dtype)
-    ##print(NumPart.dtype)
-    #print(dummy.dtype)
-    #print(suns.dtype)
+    last = _updateNodeRecursive(NumPart,-1,NumPart,last,suns,nextnode,next_node,sibling)
 
-    _updateNodeRecursive(NumPart,dummy,NumPart,last,suns,nextnode,next_node,sibling)
-
-    #print('last: ',last)
     if last >= NumPart:
         nextnode[last-NumPart] = -1
     else:
@@ -222,26 +212,165 @@ def _constructTree(pos,boxSizeSim,next_node, #father_node,
 
     return numNodes
 
-# ------------------------------------------------------------------------------
+@jit(nopython=True, nogil=True)
+def _treeSearchNumNgb(xyz,h,NumPart,boxSizeSim,pos,next_node,length,center,sibling,nextnode):
+    """ Helper routine for calcHsml(), see below. """
+    boxHalf = 0.5 * boxSizeSim
+
+    h2 = h * h
+    hinv = 1.0 / h
+
+    numNgbInH = 0
+    numNgbWeightedInH = 0.0
+
+    # 3D-normalized kernel
+    C1 = 2.546479089470  # COEFF_1
+    C2 = 15.278874536822 # COEFF_2
+    C3 = 5.092958178941  # COEFF_5
+    CN = 4.188790204786  # NORM_COEFF (4pi/3)
+
+    # start search
+    no = NumPart
+
+    while no >= 0:
+        if no < NumPart:
+            # single particle
+            #print(' single_particle: no=%d to %d' % (no,next_node[no]))
+            p = no
+            assert next_node[no] != no # Going into infinite loop.
+            no = next_node[no]
+
+            # box-exclusion along each axis
+            dx = _NEAREST( pos[p,0] - xyz[0], boxHalf, boxSizeSim )
+            if dx < -h or dx > h:
+                continue
+
+            dy = _NEAREST( pos[p,1] - xyz[1], boxHalf, boxSizeSim )
+            if dy < -h or dy > h:
+                continue
+
+            dz = _NEAREST( pos[p,2] - xyz[2], boxHalf, boxSizeSim )
+            if dz < -h or dz > h:
+                continue
+
+            # spherical exclusion if we've made it this far
+            r2 = dx*dx + dy*dy + dz*dz
+            if r2 >= h2:
+                continue
+
+            # count
+            numNgbInH += 1
+
+            # weighted count
+            numNgbWeightedInH += CN * _getkernel(hinv, r2, C1, C2, C3)
+
+        else:
+            # internal node
+            #print(' internal_node: no=%d to %d if discarded or %d if opened' % (no,sibling[no-NumPart],nextnode[no-NumPart]))
+            ind = no-NumPart # struct NODE *this = &Nodes[no];
+            no = sibling[ind] # in case the node can be discarded
+
+            #print('  evaluate opening: center [%g %g %g]' % (center[0,ind],center[1,ind],center[2,ind]))
+
+            if _NEAREST( center[0,ind] - xyz[0], boxHalf, boxSizeSim ) + 0.5 * length[ind] < -h:
+                continue
+            if _NEAREST( center[0,ind] - xyz[0], boxHalf, boxSizeSim ) - 0.5 * length[ind] > h:
+                continue
+
+            if _NEAREST( center[1,ind] - xyz[1], boxHalf, boxSizeSim ) + 0.5 * length[ind] < -h:
+                continue
+            if _NEAREST( center[1,ind] - xyz[1], boxHalf, boxSizeSim ) - 0.5 * length[ind] > h:
+                continue
+
+            if _NEAREST( center[2,ind] - xyz[2], boxHalf, boxSizeSim ) + 0.5 * length[ind] < -h:
+                continue
+            if _NEAREST( center[2,ind] - xyz[2], boxHalf, boxSizeSim ) - 0.5 * length[ind] > h:
+                continue
+
+            #print('  open')
+
+            no = nextnode[ind] # we need to open the node
+
+    return numNgbWeightedInH
 
 @jit(nopython=True, nogil=True)
-def _updateNodeRecursive2(last):
-    if last > 3:
-        return
+def _treeSearchHsmlSingle(xyz,h_guess,nNGB,nNGBDev,NumPart,boxSizeSim,pos,
+                          next_node,length,center,sibling,nextnode):
+    """ Helper routine for calcHsml(), see below. """
+    left  = 0.0
+    right = 0.0
+    one_third = 1.0 / 3.0
 
-    for i in range(4):
-        last += 1
-        _updateNodeRecursive2(last)
+    if h_guess == 0.0:
+        h_guess = 1.0
+
+    iter_num = 0
+
+    while 1:
+        iter_num += 1
+
+        assert iter_num < 1000 # Convergence failure, too many iterations.
+
+        numNgbInH = _treeSearchNumNgb(xyz,h_guess,NumPart,boxSizeSim,pos,
+                                      next_node,length,center,sibling,nextnode)
+
+        #print(' iter=%d h=%g nNGB=%g (left=%g right=%g)' % (iter_num,h_guess,numNgbInH,left,right))
+
+        # success
+        if numNgbInH > (nNGB-nNGBDev) and numNgbInH <= (nNGB+nNGBDev):
+            break
+
+        # fail, the number of neighbors we found within h_guess is outside bounds
+        if left > 0.0 and right > 0.0:
+            if right-left < 0.001 * left:
+                break # particle is OK
+
+        if numNgbInH < nNGB-nNGBDev:
+            left = max(h_guess, left)
+        else:
+            if right != 0.0:
+                if h_guess < right:
+                    right = h_guess
+            else:
+                right = h_guess
+
+        if right > 0.0 and left > 0.0:
+            h_guess = np.power( 0.5 * (left*left*left + right*right*right), one_third )
+        else:
+            assert right > 0.0 or left > 0.0 # Cannot occur that both are zero.
+
+            if right == 0.0 and left > 0.0:
+                h_guess *= 1.26
+            if right > 0.0 and left == 0.0:
+                h_guess /= 1.26
+
+    #print(' found h = %g iter_num = %d' % (h_guess,iter_num))
+    return h_guess
 
 @jit(nopython=True, nogil=True)
-def _constructTree2():
-    last = np.int32(-1)
-    _updateNodeRecursive2(last)
+def _treeSearchHsmlSet(posSearch,nNGB,nNGBDev,NumPart,boxSizeSim,pos,
+                       next_node,length,center,sibling,nextnode):
+    """ Core routine for calcHsml(), see below. """
+    numSearch = posSearch.shape[0]
 
-def testMe():
-    _constructTree2()
+    h_guess = 1.0
+    if boxSizeSim > 0.0:
+        h_guess = boxSizeSim / NumPart**(1.0/3.0)
 
-# ------------------------------------------------------------------------------
+    hsml = np.zeros( numSearch, dtype=np.float32 )
+
+    for i in range(numSearch):
+        # single ball search using octtree, requesting hsml which enclosed nNGB+/-nNGBDev around xyz
+        xyz = posSearch[i,:]
+        #print('[%d] xyz %g %g %g' % (i,xyz[0],xyz[1],xyz[2]))
+
+        hsml[i] = _treeSearchHsmlSingle(xyz,h_guess,nNGB,nNGBDev,NumPart,boxSizeSim,pos,
+                                        next_node,length,center,sibling,nextnode)
+
+        # use previous result as guess for the next (any spatial ordering will greatly help)
+        h_guess = hsml[i]
+
+    return hsml
 
 def calcHsml(pos, boxSizeSim, posSearch=None, nNGB=32, nNGBDev=1, nDims=3, treePrec='single', nThreads=1):
     """ Calculate a characteristic 'size' ('smoothing length') given a set of input particle coordinates, 
@@ -257,17 +386,18 @@ def calcHsml(pos, boxSizeSim, posSearch=None, nNGB=32, nNGBDev=1, nDims=3, treeP
       nThreads       : do multithreaded calculation (on treefind, while tree construction remains serial)
     """
     # input sanity checks
-    assert pos.ndim == 2 and pos.shape[1] in [2,3], 'Strange dimensions of pos.'
-    assert posSearch is None, 'Not implemented (separate posSearch array, need still to port from IDL).'
+    assert pos.ndim == 2 and pos.shape[1] in [3], 'Strange dimensions of pos.'
     assert pos.dtype in [np.float32, np.float64], 'pos not in float32/64.'
-    assert nDims in [1,2,3], 'Invalid ndims specification.'
+    assert nDims in [3], 'Invalid ndims specification (3D only).'
     assert treePrec in ['single','double'], 'Invalid treePrec specification.'
 
     # tree allocation and construction
     treeDtype = 'float32' if treePrec == 'single' else 'float64'
 
     NumPart = pos.shape[0]
-    MaxNodes = np.int(0.7*NumPart) + 1
+    MaxNodes = np.int(1.1*NumPart) + 1
+
+    assert NumPart >= nNGB-nNGBDev, 'Less particles then requested neighbors.'
 
     length   = np.zeros( MaxNodes, dtype=treeDtype )     # NODE struct member
     center   = np.zeros( (3,MaxNodes), dtype=treeDtype ) # NODE struct member
@@ -284,25 +414,43 @@ def calcHsml(pos, boxSizeSim, posSearch=None, nNGB=32, nNGBDev=1, nDims=3, treeP
     #FatherNode = np.zeros( NumPart, dtype='int32' )
 
     # call JIT compiled kernel
+    start_time = time.time()
+
     numNodes = _constructTree(pos,boxSizeSim,NextNode, #Father,
                               length,center,suns,sibling,nextnode) #,father)
                               #,maxsort,s,mass,bitflags) # unused
 
-    # tree search
-    print('numNodes: %d (MaxNodes=%d)' % (numNodes,MaxNodes))
+    build_done_time = time.time()
 
+    # tree search
+    #print('numNodes: %d (MaxNodes=%d)' % (numNodes,MaxNodes))
     #for i in range(numNodes):
     #    print(' [%d] length=%d [%d %d %d %d %d %d %d %d] sibling=%d nextnode=%d' % \
     #      (i, length[i], suns[0,i], suns[1,i], suns[2,i], suns[3,i], suns[4,i], 
-    #s       suns[5,i], suns[6,i], suns[7,i], sibling[i], nextnode[i]))
+    #       suns[5,i], suns[6,i], suns[7,i], sibling[i], nextnode[i]))
 
+    #print('NumPart: %d' % (NumPart))
+    #for i in range(NumPart):
+    #    print(' [%d] next_node = %d' % (i,NextNode[i]))
+
+    if posSearch is None:
+        posSearch = pos # set search coordinates as a view onto the same pos used to make the tree
+
+    hsml = _treeSearchHsmlSet(posSearch,nNGB,nNGBDev,NumPart,boxSizeSim,pos,
+                              NextNode,length,center,sibling,nextnode)
+
+    done_time = time.time()
+
+    print(' calcHsml(): tree construction took [%g] sec, search took [%g] sec.' % \
+        (build_done_time-start_time, done_time-build_done_time))
+
+    return hsml
 
 def benchmark():
     """ Benchmark performance of calcHsml(). """
     np.random.seed(424242)
     from cosmo.load import snapshotSubset
     from util.simParams import simParams
-    import time
 
     # config data
     if 1:
@@ -310,11 +458,13 @@ def benchmark():
         class sP:
             boxSize = 100.0
 
-        nPts = 2000
+        nPts = 80000
 
         posDtype = 'float32'
-
         pos = np.random.uniform(low=0.0, high=sP.boxSize, size=(nPts,3)).astype(posDtype)
+        #pos = np.zeros( (nPts,3), dtype=posDtype )
+        #pos[0,:] = [10,20,30]
+        #pos[1,:] = [40,45,10]
 
     if 0:
         # load some gas in a box
@@ -327,11 +477,15 @@ def benchmark():
     treePrec = 'single'
     nThreads = 1
 
+    # warmup (compile)
+    hsml = calcHsml(pos,sP.boxSize,nNGB=nNGB,nNGBDev=nNGBDev,treePrec=treePrec,nThreads=nThreads)
+
     # calculate and time
     start_time = time.time()
-    nLoops = 1
+    nLoops = 2
 
     for i in np.arange(nLoops):
         hsml = calcHsml(pos,sP.boxSize,nNGB=nNGB,nNGBDev=nNGBDev,treePrec=treePrec,nThreads=nThreads)
 
-    print('%d estimates of HSMLs took [%g] sec' % (nLoops,(time.time()-start_time)/nLoops))
+    print('%d estimates of HSMLs took [%g] sec on avg' % (nLoops,(time.time()-start_time)/nLoops))
+    print('hsml min %g max %g mean %g' % (np.max(hsml), np.min(hsml), np.mean(hsml)))
