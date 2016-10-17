bd794f6b8433e9603c005d9ad4757df232e7ccf0
Dylan Nelson <dnelson@cfa.harvard.edu>
2016-10-17 12:14:32 -0400

    Galaxy colors: generalizations of 2D histos and additional quantities.

--- plot/galaxyColor.py
+++ plot/galaxyColor.py
@@ -9,23 +9,23 @@ import numpy as np
 import h5py
 import pdb
 import matplotlib.pyplot as plt
-from matplotlib.colors import Normalize, LogNorm
+from matplotlib.colors import Normalize, LogNorm, colorConverter
 from matplotlib.backends.backend_pdf import PdfPages
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 from scipy.signal import savgol_filter
 from scipy.stats import binned_statistic_2d
 
 from util import simParams
-from util.helper import loadColorTable, running_median, contourf, logZeroNaN
-from cosmo.load import groupCat, groupCatSingle, auxCat, snapshotSubset
+from util.helper import loadColorTable, running_median, contourf, logZeroSafe, logZeroNaN
 from cosmo.util import cenSatSubhaloIndices
 from cosmo.stellarPop import loadSimGalColors
+from plot.general import simSubhaloQuantity
 
 # global configuration (remove duplication with globalComp.py)
 sKn     = 5   # savgol smoothing kernel length (1=disabled)
 sKo     = 3   # savgol smoothing kernel poly order
 binSize = 0.2 # dex in stellar mass/halo mass for median lines
-figsize = (16,9) # (8,6)
+figsize = (14,10) # (8,6)
 clean   = False  # make visually clean plots with less information
 
 def _bandMagRange(bands):
@@ -36,81 +36,36 @@ def _bandMagRange(bands):
     if bands[0] == 'i' and bands[1] == 'z': mag_range = [0.0,0.4]
     return mag_range
 
-def simSubhaloQuantity(sP, quant):
-    """ Return a 1D vector of size Nsubhalos, one quantity per subhalo as specified by the string 
-    cQuant, wrapping any special loading or processing. Also return an appropriate label and range. """
-    label = None
-
-    if quant == 'mstar':
-        # stellar mass (within 2r1/2stars) used for x-axis, so already returned in log
-        gc = groupCat(sP, fieldsSubhalos=['SubhaloMassInRadType'])
-        vals = sP.units.codeMassToLogMsun( gc['subhalos'][:,4] )
-
-        label = 'M$_{\\rm \star}(<2r_{\star,1/2})$ [ log M$_{\\rm sun}$ ]'
-        if clean: label = 'M$_{\\rm \star}$ [ log M$_{\\rm sun}$ ]'
-        minMax = [9.0, 12.0] # log Mstar
-
-    if quant == 'ssfr':
-        # specific star formation rate (SFR and Mstar both within 2r1/2stars)
-        gc = groupCat(sP, fieldsSubhalos=['SubhaloMassInRadType','SubhaloSFRinRad'])
-        mstar = sP.units.codeMassToMsun( gc['subhalos']['SubhaloMassInRadType'][:,4] )
-
-        # fix mstar=0 values such that vals_raw is zero, which is then masked
-        w = np.where(mstar == 0.0)[0]
-        if len(w):
-            mstar[w] = 1.0
-            gc['subhalos']['SubhaloSFRinRad'][w] = 0.0
-
-        vals = gc['subhalos']['SubhaloSFRinRad'] / mstar
-        #vals[vals == 0.0] = vals[vals > 0.0].min() * 1.0 # set SFR=0 values
-
-        label = 'log sSFR [ M$_\odot$ / yr ]'
-        minMax = [-9.0,-12.0]
-
-    if quant == 'Z_stars':
-        # mass-weighted mean stellar metallicity (within 2r1/2stars)
-        gc = groupCat(sP, fieldsSubhalos=['SubhaloStarMetallicity'])
-        vals = sP.units.metallicityInSolar(gc['subhalos'])
-
-        label = 'log ( Z$_{\\rm stars}$ / Z$_{\odot}$ )'
-        minMax = [-2.0,1.0]
-
-    if quant == 'Z_gas':
-        # mass-weighted mean gas metallicity  (within 2r1/2stars)
-        gc = groupCat(sP, fieldsSubhalos=['SubhaloGasMetallicity'])
-        vals = sP.units.metallicityInSolar(gc['subhalos'])
-
-        label = 'log ( Z$_{\\rm gas}$ / Z$_{\odot}$ )'
-        minMax = [-2.0,1.0]
-
-
-
-    assert label is not None
-    return vals, label, minMax
-
-def histo2D(sP, pdf, bands, xQuant='mstar', cenSatSelect='cen', cQuant=None, cStatistic=None, minCount=None):
+def histo2D(sP, pdf, bands, xQuant='mstar2_log', cenSatSelect='cen', cQuant=None, cStatistic=None, 
+            minCount=None, simColorsModel='p07c_bc00dust'):
     """ Make a 2D histogram of subhalos with some color on the y-axis, a property on the x-axis, 
     and optionally a third property as the colormap per bin. minCount specifies the minimum number of 
     points a bin must contain to show it as non-white. """
     assert cenSatSelect in ['all', 'cen', 'sat']
     assert cStatistic in [None,'mean','median','count','sum'] # or any user function
+    assert simColorsModel in ['snap','p07c_nodust','p07c_bc00dust']
 
     # hard-coded config
-    nBins          = 80
-    simColorsModel = 'p07c_bc00dust' # snap, p07c_nodust, p07c_bc00dust
+    nBins = 80
 
-    cmap = loadColorTable('plasma')
-    flag_color = '#000000' # for non-empty colorized bins which have e.g. median(roperty)==0
+    cmap = loadColorTable('viridis') # plasma
 
     if bands == ['u','i']: mag_range = [0.5,4.0]
     if bands == ['g','r']: mag_range = [0.15,0.85]
     if bands == ['r','i']: mag_range = [0.0,0.6]
     if bands == ['i','z']: mag_range = [0.0,0.4]
 
-    # load fullbox galaxy properties and set plot options for x-axis
-    sim_xvals, xlabel, xMinMax = simSubhaloQuantity(sP, xQuant)
+    # x-axis: load fullbox galaxy properties and set plot options
+    sim_xvals, xlabel, xMinMax = simSubhaloQuantity(sP, xQuant, clean)
+    if xMinMax[0] > xMinMax[1]: xMinMax = xMinMax[::-1] # reverse
 
-    # load properties for color mappings
+    # y-axis: load/calculate simulation colors
+    sim_colors = loadSimGalColors(sP, simColorsModel, bands=bands)
+
+    ylabel = '(%s-%s) color [ mag ]' % (bands[0],bands[1])
+    if not clean: ylabel += ' %s' % simColorsModel
+
+    # c-axis: load properties for color mappings
     if cQuant is None:
         sim_cvals = np.zeros( sim_xvals.size, dtype='float32' )
         assert cStatistic in [None,'count']
@@ -122,10 +77,7 @@ def histo2D(sP, pdf, bands, xQuant='mstar', cenSatSelect='cen', cQuant=None, cSt
         clabel = 'log N$_{\\rm gal}$'
         cMinMax = [0.0,2.0]
     else:
-        sim_cvals, clabel, cMinMax = simSubhaloQuantity(sP, cQuant)
-
-    # load/calculate simulation colors for y-axis
-    sim_colors = loadSimGalColors(sP, simColorsModel, bands=bands)
+        sim_cvals, clabel, cMinMax = simSubhaloQuantity(sP, cQuant, clean)
 
     # central/satellite selection?
     wSelect = cenSatSubhaloIndices(sP, cenSatSelect=cenSatSelect)
@@ -134,8 +86,8 @@ def histo2D(sP, pdf, bands, xQuant='mstar', cenSatSelect='cen', cQuant=None, cSt
     sim_cvals = sim_cvals[wSelect]
     sim_colors = sim_colors[wSelect]
 
-    # reduce to the subset with non-NaN galaxy colors (i.e. minimum 1 star particle)
-    wFiniteColor = np.isfinite(sim_colors)
+    # reduce to the subset with non-NaN x/y-axis values (galaxy colors, i.e. minimum 1 star particle)
+    wFiniteColor = np.isfinite(sim_colors) #& np.isfinite(sim_xvals)
     sim_colors = sim_colors[wFiniteColor]
     sim_cvals  = sim_cvals[wFiniteColor]
     sim_xvals  = sim_xvals[wFiniteColor]
@@ -147,12 +99,11 @@ def histo2D(sP, pdf, bands, xQuant='mstar', cenSatSelect='cen', cQuant=None, cSt
     ax.set_xlim(xMinMax)
     ax.set_ylim(mag_range)
     ax.set_xlabel(xlabel)
-    ax.set_ylabel('(%s-%s) color [ mag ]' % (bands[0],bands[1]))
+    ax.set_ylabel(ylabel)
 
     if not clean:
-        print(' ','-'.join(bands),xQuant,cenSatSelect,cQuant,cStatistic,minCount)
-        ax.set_title('stat=%s model=%s select=%s mincount=%s' % \
-            (cStatistic,simColorsModel,cenSatSelect,minCount))
+        print(' ','-'.join(bands),simColorsModel,xQuant,cenSatSelect,cQuant,cStatistic,minCount)
+        ax.set_title('stat=%s select=%s mincount=%s' % (cStatistic,cenSatSelect,minCount))
 
     # 2d histogram
     bbox = ax.get_window_extent()
@@ -160,29 +111,35 @@ def histo2D(sP, pdf, bands, xQuant='mstar', cenSatSelect='cen', cQuant=None, cSt
     extent = [xMinMax[0],xMinMax[1],mag_range[0],mag_range[1]]
 
     # statistic reduction (e.g. median, sum, count) color by bin
+    if '_log' not in xQuant: sim_xvals = logZeroSafe(sim_xvals) # xMinMax always corresponds to log values
+
     cc, xBins, yBins, inds = binned_statistic_2d(sim_xvals, sim_colors, sim_cvals, cStatistic, 
                                                  bins=nBins2D, range=[xMinMax,mag_range])
+
+    cc = cc.T # imshow convention
 
     # only show bins with a minimum number of points?
     if minCount is not None:
         nn, _, _, _ = binned_statistic_2d(sim_xvals, sim_colors, sim_cvals, 'count', 
                                           bins=nBins2D, range=[xMinMax,mag_range])
 
-        cc[nn < minCount] = np.nan
+        cc[nn.T < minCount] = np.nan
 
     # for now: log on density and all color quantities
-    cc2d = logZeroNaN(cc.T)
+    cc2d = logZeroNaN(cc)
 
-    # sSFR: mask bins with median==0 and map to special color 
-    if cQuant == 'ssfr':
-        # FINISH
-        cmap.set_over(flag_color, 1.0) # set over values to gray (use over instead of bad)
-        #cmap.set_under('r', 1.0) # set over values to gray (use over instead of bad)
-        #cc2d = np.ma.masked_where(cc.T == 0.0, cc2d) # empty bins/bad stay at NaN which is transparent
-
-    # method (A)
+    # normalize and color map
     norm = Normalize(vmin=cMinMax[0], vmax=cMinMax[1], clip=False)
-    plt.imshow(cc2d, extent=extent, origin='lower', interpolation='nearest', aspect='auto', 
+    cc2d_rgb = cmap(norm(cc2d))
+
+    # mask bins with median==0 and map to special color, which right now have been set to log10(0)=NaN
+    if cQuant is not None:
+        cc2d_rgb[cc == 0.0,:] = colorConverter.to_rgba('#dddddd')
+
+    # mask bins with median==NaN (empty) to white
+    cc2d_rgb[~np.isfinite(cc),:] = colorConverter.to_rgba('#ffffff')
+
+    plt.imshow(cc2d_rgb, extent=extent, origin='lower', interpolation='nearest', aspect='auto', 
                cmap=cmap, norm=norm)
 
     # method (B) unused
@@ -223,10 +180,13 @@ def histo2D(sP, pdf, bands, xQuant='mstar', cenSatSelect='cen', cQuant=None, cSt
     cb = plt.colorbar(cax=cax)
     cb.ax.set_ylabel(clabel)
 
-    # finish plot
-    #fig.save('histo2d_%s_%s_%s_%s.pdf' % ('-'.join(bands),xQuant,cQuant,cenSatSelect))
+    # finish plot and save
     fig.tight_layout()
-    pdf.savefig()
+    if pdf is not None:
+        pdf.savefig()
+    else:
+        fig.save('histo2d_%s_%s_%s_%s_%s_%s_%s.pdf' % \
+            ('-'.join(bands),simColorsModel,xQuant,cQuant,cStatistic,cenSatSelect,minCount))
     plt.close(fig)
 
 def plots():
@@ -238,13 +198,23 @@ def plots():
     #for cs in ['median','mean']:
     #    for css in ['cen','sat','all']:
     bands = ['g','r']
-    xQuant = 'mstar'
+    xQuant = 'mstar2_log'
     cs = 'median'
     css = 'cen'
 
     #histo2D(sP, pdf, bands, xQuant=xQuant, cenSatSelect=css, cQuant=None)
-    histo2D(sP, pdf, bands, xQuant=xQuant, cenSatSelect=css, cQuant='ssfr', cStatistic=cs)
+    #histo2D(sP, pdf, bands, xQuant=xQuant, cenSatSelect=css, cQuant='ssfr', cStatistic=cs)
     #histo2D(sP, pdf, bands, xQuant=xQuant, cenSatSelect=css, cQuant='Z_stars', cStatistic=cs)
     #histo2D(sP, pdf, bands, xQuant=xQuant, cenSatSelect=css, cQuant='Z_gas', cStatistic=cs)
+    #histo2D(sP, pdf, bands, xQuant=xQuant, cenSatSelect=css, cQuant='size_stars', cStatistic=cs)
+    #histo2D(sP, pdf, bands, xQuant=xQuant, cenSatSelect=css, cQuant='size_gas', cStatistic=cs)
+    #histo2D(sP, pdf, bands, xQuant=xQuant, cenSatSelect=css, cQuant='fgas1', cStatistic=cs)
+    #histo2D(sP, pdf, bands, xQuant=xQuant, cenSatSelect=css, cQuant='fgas2', cStatistic=cs)
+    #histo2D(sP, pdf, bands, xQuant=xQuant, cenSatSelect=css, cQuant='mgas2', cStatistic=cs)
+    #histo2D(sP, pdf, bands, xQuant=xQuant, cenSatSelect=css, cQuant='mgas1', cStatistic=cs)
+    histo2D(sP, pdf, bands, xQuant=xQuant, cenSatSelect=css, cQuant='stellarage', cStatistic=cs)
+
+    #histo2D(sP, pdf, bands, xQuant='ssfr', cenSatSelect=css, cQuant=None)
+    #histo2D(sP, pdf, bands, xQuant='ssfr', cenSatSelect=css, cQuant='mstar2', cStatistic=cs)
 
     pdf.close()
