3776ba262c9254c54a6a878214c02a0bf879b148
Dylan Nelson <3587498+dnelson86@users.noreply.github.com>
2016-11-24 16:35:26 +0100

    Cosmo.util.plotMassFunctions()

--- cosmo/util.py
+++ cosmo/util.py
@@ -571,3 +571,57 @@ def plotRedshiftSpacings():
 
     fig.tight_layout()    
     fig.savefig(sP.plotPath + 'redshift_spacing.pdf')
+    plt.close(fig)
+
+def plotMassFunctions():
+    import matplotlib.pyplot as plt
+    from util import simParams
+
+    # config
+    mass_ranges = [ [6.6, 16.0], [6.6, 13.0] ] # m_halo, m_star
+    binSize = 0.2
+    
+    sPs = []
+    sPs.append( simParams(res=1820,run='tng',redshift=2.0) )
+    sPs.append( simParams(res=2500,run='tng',redshift=2.0) )
+
+    # plot setup
+    fig = plt.figure(figsize=(18,8))
+
+    # halo or stellar mass function
+    for j, mass_range in enumerate(mass_ranges):
+        nBins = (mass_range[1]-mass_range[0])/binSize
+
+        ax = fig.add_subplot(1,2,j+1)
+        ax.set_xlim(mass_range)
+        if j == 0: ax.set_xlabel('Halo Mass [ M$_{\\rm 200,crit}$  log M$_\odot$ ]')
+        if j == 1: ax.set_xlabel('Stellar Mass [ M$_\star(<2r_{\\rm 1/2,stars})$  centrals  log M$_\odot$ ]')
+        ax.set_ylabel('N$_{\\rm bin=%.1f}$' % binSize)
+        ax.set_xticks(np.arange(np.int32(mass_range[0]),np.int32(mass_range[1])+1))
+        ax.set_yscale('log')
+
+        yy_max = 1.0
+
+        for i, sP in enumerate(sPs):
+            print(j,sP.simName)
+
+            if j == 0:
+                gc = cosmo.load.groupCat(sP, fieldsHalos=['Group_M_Crit200'])
+                masses = sP.units.codeMassToLogMsun(gc['halos'])
+            if j == 1:
+                gc = cosmo.load.groupCat(sP, fieldsHalos=['GroupFirstSub'], fieldsSubhalos=['SubhaloMassInRadType'])
+                masses = gc['subhalos'][ gc['halos'] ][:,sP.ptNum('stars')] # Mstar (<2*r_{1/2,stars})
+                masses = sP.units.codeMassToLogMsun(masses)
+
+            yy, xx = np.histogram(masses, bins=nBins, range=mass_range)
+            yy_max = np.max([yy_max,yy.max()])
+
+            label = sP.simName + ' z=%.1f' % sP.redshift
+            ax.hist(masses,bins=nBins,range=mass_range,lw=2.0,label=label,histtype='step',alpha=0.9)
+
+        ax.set_ylim([1,yy_max*1.4])
+        ax.legend()
+
+    fig.tight_layout()    
+    fig.savefig('mass_functions.pdf')
+    plt.close(fig)
