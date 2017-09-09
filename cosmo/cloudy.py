c572f214e32d47e2079fe588ee2eb7a6dcfdedcf
Dylan Nelson <dnelson@mpa-garching.mpg.de>
2017-09-09 13:35:50 +0200

    calcGasMetalAbundances() handle mini-snaps gracefully.

--- cosmo/cloudy.py
+++ cosmo/cloudy.py
@@ -671,13 +671,16 @@ class cloudyIon():
         else:
             # note: GFM_Metals[X] is the mass ratio of each element to total gas mass (M_X/M_gas)
             # so we can use, as long as the requested element X is one of the 9 tracked GFM elements
-            assert snapHasField(sP, 'gas', 'GFM_Metals')
-            assert self.elementNameToSymbol(element) in sP.metals
+            if snapHasField(sP, 'gas', 'GFM_Metals'):
+                assert self.elementNameToSymbol(element) in sP.metals
 
-            fieldName = "metals_" + self.elementNameToSymbol(element)
-            metal_mass_fraction = snapshotSubset(sP, 'gas', fieldName, indRange=indRange)
+                fieldName = "metals_" + self.elementNameToSymbol(element)
+                metal_mass_fraction = snapshotSubset(sP, 'gas', fieldName, indRange=indRange)
 
-            metal_mass_fraction[metal_mass_fraction < 0.0] = 0.0 # clip -eps values at zero
+                metal_mass_fraction[metal_mass_fraction < 0.0] = 0.0 # clip -eps values at zero
+            else:
+                print('WARNING: metal abunds but GFM_Metals not available (mini-snap). Assuming solar abundances.')
+                metal_mass_fraction = (metal/self.solar_Z) * self._solarMetalAbundanceMassRatio(element)
 
         metal_ion_mass_fraction = metal_mass_fraction * ion_fraction
         
