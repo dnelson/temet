93ce3c09148e1ded52a56e16ba11cf9ba541248b
Dylan Nelson <dnelson@mpa-garching.mpg.de>
2019-01-09 14:39:53 +0100

    Shape finding algorithm based on Eddie's code (iterative ellipsoid fitting).

--- util/rotation.py
+++ util/rotation.py
@@ -267,3 +267,263 @@ def rotateCoordinateArray(sP, pos, rotMatrix, rotCenter, shiftBack=True):
     pos_in = np.asarray(pos_in) # matrix to ndarray
 
     return pos_in, extent
+
+def ellipsoidfit(pos_in, mass, scalerad, rin, rout, weighted=False):
+    """ Iterative algorithm to derive the shape (axes ratios) of a set of particles/cells given by pos_in and mass, 
+    within a radial shell defined by rin, rout. Positions should be unitless, normalized by scalerad (a scaling factor, 
+    could be the virial radius or e.g. a half mass radius). """
+    pos = pos_in.copy()
+    assert pos.shape[0] == mass.size
+
+    convcrit = 1e-2 # convergence criterion
+    minNumParticles = 20 # quit if we drop below this
+    minEigenval = 1e-4 # quit if we drop below this
+
+    q = 1.0
+    s = 1.0
+
+    conv = 10
+    count = 0
+
+    axes = np.eye(3) # holds final rotation
+
+    # iterate until convergence or until an early exit
+    while conv > convcrit:
+        count += 1
+
+        # restrict to particles within required radii
+        distsq = pos[:,0]**2 + (pos[:,1]/q)**2 + (pos[:,2]/s)**2
+        w = np.where( (distsq > rin**2) & (distsq <= rout**2) )
+
+        if len(w[0]) < minNumParticles:
+            #print('early exit, particle num dropped to [%d]' % len(w[0]))
+            break
+
+        if len(w[0]) == 0:
+            print('no particles in bin!')
+            assert 0 # check
+            break
+
+        pos_loc = pos[w[0],:]
+        mass_loc = mass[w]
+
+        # use weighted shape tensor?
+        a2 = 1.0
+
+        if weighted:
+            a2 = pos_loc[:,0]**2 + (pos_loc[:,1]/q)**2 + (pos_loc[:,2]/s)**2
+
+        # calculate shape tensor
+        M = np.zeros( (3,3), dtype='float32' )
+
+        for i in range(3):
+            M[i,i] += np.sum(mass_loc * pos_loc[:,i]**2 / a2)
+
+            for j in range(i): # range(3) ??
+                val = np.sum(mass_loc * pos_loc[:,i]*pos_loc[:,j] / a2)
+                M[i,j] += val
+                M[j,i] += val
+
+        # normalize
+        M *= scalerad**2 / np.sum(mass_loc)
+
+        # get (normalized) eigenvectors and corresponding eigenvalues, sorting from largest to smallest
+        eigvals, eigvecs = np.linalg.eigh(M)
+
+        sort_inds = np.argsort(eigvals)[::-1]
+        eigvals = eigvals[sort_inds]
+        eigvecs = eigvecs[:,sort_inds] # columns are the principal axes
+
+        rot_matrix = eigvecs.T # actual rotation matrix is the transpose of this, since the transformation is I'= R I R.T
+
+        # check rotation matrix using similarity transformation
+        # the convention is M' = V.T M V, which should correspond to the eigenvalues on the diagonal
+        if not np.allclose( np.dot(eigvecs.T, np.dot(M,eigvecs)), np.diag(eigvals), atol=1e-4):
+            print('Issue: ', np.dot(eigvecs.T, np.dot(M,eigvecs)), ' vs ', eigvals)
+            assert 0 # check
+            break
+
+        # are any of the eigenvalues very small? the next iteration will then return imaginary values
+        if (eigvals < minEigenval).any():
+            #print('eigenvalues too small, stopping early')
+            break
+
+        if (eigvals < 1e-6).any():
+            print('Eigenvalues too small, stopping.')
+            assert 0 # check
+            break
+
+        # obtain q and s from the eigenvalues
+        q_new = np.sqrt(eigvals[1]/eigvals[0])
+        s_new = np.sqrt(eigvals[2]/eigvals[0])
+
+        # constraint: q_new+s_new=1.0 (or something reasonable)
+        #q_new *= 1.0/(q_new + s_new)
+
+        # rotate positions
+        pos = np.dot(pos, rot_matrix.T)
+
+        # rotate the identity matrix as well, so that we can obtain the final rotation from the original frame
+        axes = np.dot(axes, rot_matrix.T)
+
+        # check convergence
+        conv = np.max( [np.abs((q_new-q)/q), np.abs((s_new-s)/s)] )
+
+        q, s = q_new, s_new
+
+        if count > 100:
+            #print(' reached 100 iters.')
+            break
+
+        if q > 1.0 or s > 1.0:
+            print('Either q or s is larger than one, not allowed.')
+            assert 0 # check
+
+        #print(' [%2d] q = %.3f s = %.3f ratio=%.2f n = %5d conv = %.3f' % (count,q,s,s/q,mass_loc.size,conv))
+
+        # iterations done
+        assert np.allclose( np.dot(pos_in, axes), pos )
+
+    return q, s, mass_loc.size, axes
+    
+def ellipsoidfit_run():
+    """ Find the shape (in stars, gas, or DM) of a set of galaxies/halos using the iterative ellipsoid method. """
+    from util.simParams import simParams
+
+    sP = simParams(res=2160, run='tng', redshift=2.0)
+
+    # field config
+    ptType = 'gas' # 'stars'
+    massField = 'mass' # 'sfr'
+
+    # bin config
+    solid  = False # otherwise shell
+    binwidth = 0.1
+
+    if 0:
+        # generate a number of bins from the halo center to scalerad
+        nbins = 10
+        rmin  = 1e-2 # linear, fractions of scalerad
+        rmax  = 1.0  # linear, fractions of scalerad
+
+        logr = np.linspace( np.log10(rmin), np.log10(rmax), nbins)
+
+        rin  = 10**(logr - binwidth/2.0)
+        rout = 10**(logr + binwidth/2.0)
+
+    if 1:
+        # or: use exact 'input' bin midpoints
+        rbins = np.array([1.9,2.1]) # np.array([0.5,1.0,2.0,4.0])
+        logr  = np.log10( rbins )
+        nbins = len(rbins)
+
+        rin  = rbins - binwidth/2.0
+        rout = rbins + binwidth/2.0
+
+    # object choice
+    subhaloID = 8069
+
+    shIDs_z2_final25 = [29443,79350,60750,8069,57099,
+                        68178,110543,90627,55107,102285,
+                        113349,121252,125841,115247,115582,
+                        127580,132290,130665,129661,139177,
+                        145492,146306,154635,189521,246343]
+
+    for subhaloID in shIDs_z2_final25:
+        # load a normalization/scale radius
+        #scalerad = sP.groupCatSingle(haloID=groupID)['Group_R_Crit200']
+        scalerad = sP.groupCatSingle(subhaloID=subhaloID)['SubhaloHalfmassRadType'][sP.ptNum('stars')]
+
+        # load: halo properties and member-particles (scope is subhalo only)
+        pos = sP.snapshotSubset(ptType, 'pos_rel', subhaloID=subhaloID)
+        pos /= scalerad
+        mass = sP.snapshotSubset(ptType, massField, subhaloID=subhaloID)
+
+        # for gas, filter to SFR>0 gas, and in general filter out all zero-weighted particles (for speed)
+        if ptType == 'gas':
+            sfr = sP.snapshotSubset(ptType, 'sfr', subhaloID=subhaloID)
+            w = np.where(sfr > 0.0)
+            mass = mass[w]
+            pos = pos[w[0],:]
+
+        w = np.where(mass > 0.0)
+        mass = mass[w]
+        pos = pos[w[0],:]
+
+        # allocate
+        q = np.zeros(nbins, dtype='float32')
+        s = np.zeros(nbins, dtype='float32')
+        n = np.zeros(nbins, dtype='int32')
+
+        axes = np.zeros( (nbins,3,3), dtype='float32')
+
+        # fit (multiple radial shells)
+        for i in range(nbins):
+            if solid:
+                ret = ellipsoidfit(pos, mass, scalerad, 0.0, 10**logr[i], weighted=True)
+            else:
+                ret = ellipsoidfit(pos, mass, scalerad, rin[i], rout[i])
+
+            q[i], s[i], n[i], axes[i] = ret
+
+            print('[%6d %s] [r = %.2f] q = %.3f s = %.3f ratio = %.2f' % (subhaloID,ptType,10.0**logr[i],q[i],s[i],s[i]/q[i]))
+
+
+def ellipsoidfit_test(N=1000, h=0.1, R=10.0, q=1.0, s=1.0, phi=0.0, theta=0.0, noise_frac=0.0):
+    """ Generate random points in an ellipsoidal shell and test the iterative fitting algorithm. """
+    allphi   = np.random.rand(N)*2.0*np.pi
+    costheta = 2.0*np.random.rand(N)-1.0
+    alltheta = np.arccos(costheta)
+
+    r = np.zeros(N)
+    tot = 0
+
+    while tot < N:
+        u = np.random.rand(1)
+        t = R* u**(1.0/3.0)
+        if t > (1-h)*R:
+            r[tot] = t
+            tot += 1          
+
+    x = R*np.sin(alltheta)*np.cos(allphi)
+    y = q*R*np.sin(alltheta)*np.sin(allphi)
+    z = s*R*np.cos(alltheta)
+
+    # rotate ellipsoid
+    phi   = np.deg2rad(phi)
+    theta = np.deg2rad(theta)
+    Rp    = np.array([[np.cos(phi),-np.sin(phi),0.0],[np.sin(phi),np.cos(phi),0.0],[0.0,0.0,1.0]])
+    Rt    = np.array([[1.0,0.0,0.0],[0,np.cos(theta),-np.sin(theta)],[0.0,np.sin(theta),np.cos(theta)]])
+    Rtot  = Rt.dot(Rp)
+
+    pos = np.vstack([x,y,z]).T
+    pos = np.dot(Rtot,pos.T).T
+
+    # add noise
+    noise = np.random.normal( loc=0.0, scale=noise_frac, size=pos.shape )
+    pos += noise
+
+    # fit
+    rvir = 1.0
+    mass = np.ones(N, dtype='float32')
+    rin  = 0.0
+    rout = 1e10
+
+    out = ellipsoidfit(pos, mass, rvir, rin, rout, weighted=False)
+
+    print('q=',out[0],'expected=',q)
+    print('s=',out[1],'expected=',s)
+    print('R=',out[3])
+    print(Rtot)
+    rdot = [out[3][:,i].dot(Rtot[:,i]) for i in range(3)]
+    print(rdot)
+
+def ellipsoidfit_test_run():
+    """ Execute some tests. """
+    N = 5000
+
+    test(N,q=1.0,s=1.0)
+    test(N,q=0.2,s=1.0,noise_frac=0.1)
+    test(N,q=0.2,s=1.0,phi=45.0,theta=0.0,noise_frac=0.0)
+    test(N,q=0.2,s=1.0,phi=45.0,theta=0.0,noise_frac=0.1)
+    test(N,q=0.2,s=1.0,phi=30.0,theta=45.0,noise_frac=0.1)
