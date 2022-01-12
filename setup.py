4645f1b8a5c4c17ccbdd1f7775b65d1c3e93497f
Chris Byrohl <cbyrohl@workstation-cbyrohl.MPA-Garching.MPG.DE>
2022-01-12 10:49:52 +0100

    add illustris_python dependency

--- setup.py
+++ setup.py
@@ -7,5 +7,7 @@ setup(
     install_requires=["astropy","cmocean","corner","emcee","fsps","h5py",
                       "healpy","llvmlite","matplotlib","numba","numpy",
                       "psutil","requests","requests-oauthlib","scipy",
-                      "astro-sedpy","astro-prospector","reproject"],
+                      "astro-sedpy","astro-prospector","reproject",
+                      "illustris_python @ git+https://github.com/cbyrohl/illustris_python@master",
+                      ],
 )
