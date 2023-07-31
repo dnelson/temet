Vis: sphMap_globalZoomOrig index off by one bugfix.

tenet/vis/common.py
@@ -687,7 +687,7 @@ def loadMassAndQuantity(sP, partType, partField, rotMatrix, rotCenter, method, w
         # distribute 'mass' and calculate column/volume density grid
         quant = None
 
-        if partField != 'mass':
+        if partField != 'mass' and 'coldens' not in partField:
             assert mass.sum() != massTot, 'Error! Mass array not replaced by [%s]!' % partField
 
         if partFieldLoad in volDensityFields+colDensityFields or \
@@ -1386,8 +1386,8 @@ def gridBox(sP, method, partType, partField, nPixels, axes, projType, projParams
 
             origZoomID = sP.groupCatSingle(subhaloID=sP.subhaloInd)['SubhaloOrigHaloID']
             origZoomInd = np.where(origIDs == origZoomID)[0][0]
-            indRange = [offsets[origZoomInd,pt], offsets[origZoomInd,pt]+lengths[origZoomInd,pt]]
-            indRange2 = [offsets2[origZoomInd,pt], offsets2[origZoomInd,pt]+lengths2[origZoomInd,pt]]
+            indRange = [offsets[origZoomInd,pt], offsets[origZoomInd,pt]+lengths[origZoomInd,pt]-1]
+            indRange2 = [offsets2[origZoomInd,pt], offsets2[origZoomInd,pt]+lengths2[origZoomInd,pt]-1]
             nChunks = 2
 
         if indRange is not None and indRange[1] - indRange[0] < 1: