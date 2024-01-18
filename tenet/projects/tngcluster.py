Kinetic SZ computation and vis. Bugfix: missing little-h factor in tSZ y-parameter.

tenet/projects/tngcluster.py
@@ -158,6 +158,13 @@ def vis_gallery(sP, conf=0, num=20):
         size = 400.0
         sizeType = 'kpc'
         labelScale = True
+    if conf == 12:
+        partField = 'sz_yparam'
+        valMinMax = [-8.0, -4.5]
+        contourLevels = [-7.0,7.0]
+        contourSmooth = 2
+        size = 6.0 # times rvir
+        nPixels = 1000 # square aspect ratio
 
     # targets
     pri_target = sP.groups('GroupPrimaryZoomTarget')
@@ -184,6 +191,23 @@ def vis_gallery(sP, conf=0, num=20):
         nRows      = int(np.ceil(num/nCols))
 
         saveFilename = './gallery_%s_%d_%s-%s_n%d.pdf' % (sP.simName,sP.snap,partType,partField,num)
+
+    if conf == 12:
+        # tSZ and kSZ side-by-side
+        ksz_vmm = [-9.0,9.0] # [-5e-6,5e-6] # linear vs +/-log
+        panels.append({'partField':'ksz_yparam', 'subhaloInd':subID, 'valMinMax':ksz_vmm})
+
+        plotConfig.nCols = 2
+
+        panels[0]['contour'] = ['gas','sz_yparam']
+        panels[1]['contour'] = ['gas','ksz_yparam']
+
+        panels[0]['labelHalo'] = 'mhalo,id'
+        panels[0]['labelSim'] = True
+        panels[1]['labelScale'] = True
+        panels[1]['labelZ'] = True
+
+        #plotConfig.plotStyle = 'open'
 
     renderSingleHalo(panels, plotConfig, locals(), skipExisting=False)
 
@@ -1310,12 +1334,12 @@ def szy_vs_halomass(sPs):
         ax2.fill_between(xx, y_lower, y_upper, color='#000000', alpha=0.3, zorder=-3, label='Jimeno+ (2018) Planck')
 
         # Hill+ 2018
-        #M500 = np.array([1.0e12, 1.0e13, 6.63e13, 2.44e14, 5.59e14, 1.0e15])
-        #Y500 = np.array([2.845e-9, 5.687e-7, 5.381e-5, 5.179e-4, 2.218e-3, 6.074e-3]) # arcmin^2 at z=0.15
-        #Y500 *= 3600 # arcsec^2
-        #Y500 *= sPs[0].units.arcsecToAngSizeKpcAtRedshift(1.0, 0.15)**2 / 1e6 # convert to pMpc^2
+        M500 = np.array([1.0e12, 1.0e13, 6.63e13, 2.44e14, 5.59e14, 1.0e15])
+        Y500 = np.array([2.845e-9, 5.687e-7, 5.381e-5, 5.179e-4, 2.218e-3, 6.074e-3]) # arcmin^2 at z=0.15
+        Y500 *= 3600 # arcsec^2
+        Y500 *= sPs[0].units.arcsecToAngSizeKpcAtRedshift(1.0, 0.15)**2 / 1e6 # convert to pMpc^2
 
-        #ax2.plot(np.log10(M500), np.log10(Y500), ':', color='#000000', alpha=0.7, label='Hill+ (2018) CB')
+        ax2.plot(np.log10(M500), np.log10(Y500), ':', color='#000000', alpha=0.7, label='Hill+ (2018) CB')
 
         # self-similar slope
         M500 = np.array([1.5e14, 5e14])
@@ -1330,13 +1354,13 @@ def szy_vs_halomass(sPs):
         n19 = nagarajan19()
 
         #ax.plot(h21['m500'], h21['sz_y'], 'p', color='#000000', ms=6, alpha=0.7)
-        #ax.plot(p13['M500'], p13['Y500'], '*', color='#000000', ms=6, alpha=0.7)
+        ax.plot(p13['M500'], p13['Y500'], '*', color='#000000', ms=6, alpha=0.7)
         #ax.plot(b15['M500'], b15['Y'], 's', color='#000000', ms=6, alpha=0.7)
 
         xerr = np.vstack((n19['M500_errdown'],n19['M500_errup']))
         yerr = np.vstack((n19['Y_errup'],n19['Y_errdown']))
         ax2.errorbar(n19['M500'], n19['Y'], xerr=xerr, yerr=yerr, fmt='D', zorder=-2, 
-                     color='#aaaaaa', ms=6, alpha=0.7, label=n19['label'])
+                     color='#888888', ms=6, alpha=0.7, label=n19['label'])
         
         # Adam+2023 XXL Survey - Fig 8 right panel (NIKA2 points, direct NFW mass modeling, also from other authors)
         a22_label     = 'Adam+ (2023)'
@@ -1349,8 +1373,8 @@ def szy_vs_halomass(sPs):
 
         xerr = np.vstack((a23_m500_err1 - a23_m500,a23_m500 - a23_m500_err2))
         yerr = np.vstack((a23_y500_err1 - a23_y500,a23_y500 - a23_y500_err2))
-        #ax2.errorbar(a23_m500, a23_y500, xerr=xerr, yerr=yerr, fmt='s', zorder=-2, 
-        #             color='#444444', ms=6, alpha=0.7, label=a22_label)
+        ax2.errorbar(a23_m500, a23_y500, xerr=xerr, yerr=yerr, fmt='s', zorder=-2, 
+                     color='#444444', ms=6, alpha=0.7, label=a22_label)
 
         # second legend
         ax2.legend(loc='lower right')
@@ -2017,6 +2041,7 @@ def paperPlots():
         for conf in range(11):
             vis_gallery(TNG_C, conf=conf, num=1) # single
         vis_gallery(TNG_C, conf=1, num=72) # gallery
+        #vis_gallery(TNG_C, conf=12, num=1) # AtLAST White Paper for Aurora
 
     # figure 7 - gas fractions
     if 0:

tenet/vis/common.py
@@ -33,7 +33,7 @@ colDensityFields  = ['coldens','coldens_msunkpc2','coldens_msunckpc2','coldens_s
                      'xray','xray_lum','xray_lum_05-2kev','xray_lum_0.5-2.0kev','xray_lum_0.5-5.0kev',
                      'p_sync_ska','coldens_msun_ster','sfr_msunyrkpc2','sfr_halpha','halpha',
                      'H2_BR','H2_GK','H2_KMT','HI_BR','HI_GK','HI_KMT']
-totSumFields      = ['mass','sfr','tau0_MgII2796','tau0_MgII2803','tau0_LyA','tau0_LyB','sz_yparam']
+totSumFields      = ['mass','sfr','tau0_MgII2796','tau0_MgII2803','tau0_LyA','tau0_LyB','sz_yparam','ksz_yparam']
 velLOSFieldNames  = ['vel_los','velsigma_los']
 velCompFieldNames = ['vel_x','vel_y','vel_z','bfield_x','bfield_y','bfield_z']
 haloCentricFields = ['tff','tcool_tff','menc','specangmom_mag','vrad','vrel','delta_rho']
@@ -47,7 +47,7 @@ def validPartFields(ions=True, emlines=True, bands=True):
               'coldens','coldens_msunkpc2','coldens_msunckpc2','coldens_msun_ster',
               'ionmassratio_OVI_OVII',# (generalize),
               'HI','HI_segmented','H2_BR','H2_GK','H2_KMT','HI_BR','HI_GK','HI_KMT',
-              'xray','xray_lum','sz_yparam','sfr_halpha','halpha','p_sync_ska',
+              'xray','xray_lum','sz_yparam','ksz_yparam','sfr_halpha','halpha','p_sync_ska',
               'temp','temperature','temp_sfcold',
               'ent','entr','entropy',
               'bmag','bmag_uG','bfield_x','bfield_y','bfield_z',
@@ -585,7 +585,7 @@ def loadMassAndQuantity(sP, partType, partField, rotMatrix, rotCenter, method, w
     if 'tau0_' in partField:
         mass = sP.snapshotSubsetP(partType, partField, indRange=indRange)
 
-    if partField in ['sz_yparam','p_sync_ska']:
+    if partField in ['sz_yparam','ksz_yparam','p_sync_ska']:
         mass = sP.snapshotSubsetP(partType, partField, indRange=indRange)
 
     # flux/surface brightness (replace mass)
@@ -944,8 +944,31 @@ def gridOutputProcess(sP, grid, partType, partField, boxSizeImg, nPixels, projTy
         pxAreaKpc2 = np.prod(sP.units.codeLengthToKpc(pxSizesCode))
         grid /= pxAreaKpc2
         
-        config['label'] = 'Thermal Sunyaev-Zeldovich y-parameter [log]'
+        config['label'] = 'Thermal Sunyaev-Zeldovich y-param [log]'
         config['ctName'] = 'turbo'
+
+    if partField in ['ksz_yparam']:
+        # 'per-cell kinetic yparam' has [kpc^2] units, normalize by pixel area -> dimensionless
+        pxSizesCode = [boxSizeImg[0] / nPixels[0], boxSizeImg[1] / nPixels[1]]
+        pxAreaKpc2 = np.prod(sP.units.codeLengthToKpc(pxSizesCode))
+        grid /= pxAreaKpc2
+        
+        config['label'] = 'Kinetic Sunyaev-Zeldovich y-param'
+        config['ctName'] = 'Spectral' # diverging
+
+        if 1:
+            # custom +/- log scale (note: values are small << 1.0, e.g. 1e-4 -> 4.0, -1e-4 -> -4.0)
+            w_neg = np.where(grid <= 0.0)
+            w_pos = np.where(grid > 0.0)
+            grid[w_pos] = -logZeroMin( grid[w_pos] )
+            grid[w_neg] = logZeroMin( -grid[w_neg] )
+            config['label'] += ' [$\pm$log]'
+        if 0:
+            # asinh scale (similar to symlog)
+            grid = np.arcsinh(grid)
+            config['label'] += ' [asinh]'
+
+        logMin = False
 
     if 'metals_' in partField:
         # all of GFM_Metals as well as GFM_MetalsTagged (projected as column densities)
@@ -2093,6 +2116,9 @@ def gridBox(sP, method, partType, partField, nPixels, axes, projType, projParams
         grid_master = logZeroMin( 10.0**grid_master / 10.0**grid_totmass )
         data_grid = logZeroMin( 10.0**data_grid / 10.0**data_grid_totmass )
 
+    # temporary: protect kSZ LOS
+    if partField == 'ksz_yparam': assert axes[0] == 0 and axes[1] == 1
+
     # temporary: ayromlou baryon fraction map by summing gas+stars coldens, then normalizing by gas+stars+DM
     if 0 and partField == 'coldens' and partType == 'gas':
         print('NOTE: Converting gas coldens map to baryon fraction map!')
@@ -2890,6 +2916,7 @@ def addCustomColorbars(fig, ax, conf, config, heightFac, barAreaBottom, barAreaT
     colorsB = ['white','white','white','black','black']
 
     formatStr = "%.1f" if np.max(np.abs(valLimits)) < 100.0 else "%d"
+    if np.max(np.abs(valLimits)) < 0.01: formatStr = '%.0e'
 
     cax.text(0.0+lOffset, textMidY, formatStr % (1.0*valLimits[0]+0.0*valLimits[1]), 
         color=colorsB[0], size=conf.fontsize, ha='left', va='center', transform=cax.transAxes)