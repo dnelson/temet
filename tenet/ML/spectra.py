94960dd2c5e96ead0bdb1b78a0f79847d89712bf
Dylan Nelson <dnelson@uni-heidelberg.de>
2024-03-25 17:04:31 +0100

    ML: dynamic numbers of layers, noise addition for data augmentation.

--- tenet/ML/spectra.py
+++ tenet/ML/spectra.py
@@ -5,6 +5,7 @@ import numpy as np
 import h5py
 import matplotlib.pyplot as plt
 from os.path import isfile
+from collections import OrderedDict
 
 import torch
 from torch import nn
@@ -20,11 +21,21 @@ from tenet.ML.common import train_model, test_model
 
 class MockSpectraDataset(Dataset):
     """ A custom dataset for loading mock spectra and corresponding labels. """
-    def __init__(self, simname, redshift, ion, instrument, model_type, EW_minmax=None):
+    def __init__(self, simname, redshift, ion, instrument, model_type, EW_minmax=None, 
+                 SNR=None, num_noisy_per_sample=1):
         self.sim = simParams(simname, redshift=redshift)
         self.ion = ion
         self.instrument = instrument
         self.model_type = model_type
+
+        # noise data augmentation
+        self.SNR = SNR
+        self.num_noisy_per_sample = num_noisy_per_sample
+
+        if self.SNR is not None:
+            self.rng = np.random.default_rng(424242)
+        else:
+            assert self.num_noisy_per_sample == 1 # disabled
 
         # load data
         mode = 'all' #'random' # for testing
@@ -32,7 +43,7 @@ class MockSpectraDataset(Dataset):
         solar = False # fixed
         dv_window = 1000.0 # +/- km/s
 
-        cacheFilename = 'cache_mockspec_%s-%d_%s-%s_%s_%s_%s-%s_%.0f.hdf5' % \
+        cacheFilename = 'mockspec_cache_%s-%d_%s-%s_%s_%s_%s-%s_%.0f.hdf5' % \
             (self.sim.simName, self.sim.snap, ion.replace(' ',''), instrument, str(EW_minmax).replace(' ',''), mode, num, solar, dv_window)
         
         if isfile(cacheFilename):
@@ -56,7 +67,18 @@ class MockSpectraDataset(Dataset):
                 f['wave'] = wave
                 f['lineNames'] = self.lineNames.encode('ascii')
 
-        # store samples (mstar) and labels (mhalo), only within the selected range
+        # pre-compute and store noisy samples
+        if self.SNR is not None:
+            flux_noisy = np.tile(flux, (self.num_noisy_per_sample,1))
+            noise = self.rng.normal(loc=0.0, scale=1/self.SNR, size=flux_noisy.shape)
+            flux_noisy += noise.astype('float32') # float64 -> float32
+            flux_noisy = np.clip(flux_noisy, 0, np.inf) # clip min at zero
+
+            # store (tile: [0, 1, ..., N] -> [0, 1, ..., N, 0, 1, ..., N, ...])
+            flux = flux_noisy
+            EW = np.tile(EW, self.num_noisy_per_sample)
+
+        # store samples (1d flux vectors) and labels (EW), only within the selected range
         self.samples = torch.from_numpy(flux)
         self.labels = torch.from_numpy(EW)
 
@@ -81,13 +103,29 @@ class MockSpectraDataset(Dataset):
         self.target_invtransform = target_invtransform
 
     def __len__(self):
-        # TODO: add data augmentation with random noise generation, making this len(self.samples) * num_noisy_per_sample
+        # data augmentation via (on demand) random noise addition
+        #return len(self.samples) * self.num_noisy_per_sample
+        # data augmentation via explicit (stored) random noise addition
         return len(self.samples)
 
     def __getitem__(self, i):
         """ Return a single sample at index i."""
+        # on demand augmentation: derive actual data index
+        #if i >= len(self.samples):
+        #    assert self.num_noisy_per_sample > 1
+        #    assert i < self.__len__()
+        #    i = i % len(self.samples)
+
         vals = self.samples[i,:]
         label = self.labels[i]
+
+        # add random noise to the spectrum
+        # actually fairly slow, better to pre-compute and store all noisy samples?
+        #if self.SNR is not None:
+        #    noise = self.rng.normal(loc=0.0, scale=1/self.SNR, size=vals.shape)
+        #    vals += noise.astype('float32') # float64 -> float32
+        #    # achieved SNR = 1/stddev(noise)
+        #    vals = np.clip(vals, 0, np.inf) # clip negative values at zero
 
         if self.target_transform:
             label = self.target_transform(label)
@@ -101,25 +139,31 @@ class MockSpectraDataset(Dataset):
 
 class mlp_network(nn.Module):
     """ Simple MLP NN to explore the (normalized absorption spectra) -> (EW) mapping. """
-    def __init__(self, hidden_size, num_inputs):
+    def __init__(self, hidden_size, num_inputs, num_hidden_layers=1):
         """
         Args:
           hidden_size (int): number of neurons in the hidden layer(s).
           num_inputs (int): number of input features i.e. number of wavelength bins.
         """
         super().__init__()
+        self.num_hidden_layers = num_hidden_layers
         self.hidden_size = hidden_size
         self.num_inputs = num_inputs
 
-        # define layers
+        # define layers (ordered dict)
+        layers = OrderedDict()
+
+        layers['lin0'] = nn.Linear(self.num_inputs, self.hidden_size) # # input/first layer, w*x + b
+        layers['lin0_relu'] = nn.ReLU()
+
+        for i in range(self.num_hidden_layers):
+            layers[f'lin{i+1}'] = nn.Linear(self.hidden_size, self.hidden_size)
+            layers[f'lin{i+1}_relu'] = nn.ReLU()
+
+        layers['lin_final'] = nn.Linear(self.hidden_size, 1) # output layer
+        
         # Sequential = ordered container of modules (data passed through each module in order)
-        self.linear_relu_stack = nn.Sequential(
-            nn.Linear(self.num_inputs, self.hidden_size), # input/first layer, w*x + b
-            nn.ReLU(), 
-            nn.Linear(self.hidden_size, self.hidden_size), # hidden layer, fully connected
-            nn.ReLU(),
-            nn.Linear(self.hidden_size, 1), # output layer
-        )
+        self.linear_relu_stack = nn.Sequential(layers)
 
         # diagnostics
         self.total_num_params = sum(p.numel() for p in self.parameters())
@@ -130,12 +174,13 @@ class mlp_network(nn.Module):
     
 class cnn_network(nn.Module):
     """ Simple CNN to explore the (normalized absorption spectra) -> (EW) mapping. """
-    def __init__(self, kernel_size, hidden_size, num_inputs):
+    def __init__(self, kernel_size, hidden_size, num_inputs, num_hidden_layers=1):
         """
         Args:
           kernel_size (int): size of convolution kernel.
           hidden_size (int): number of neurons in the hidden layer(s).
           num_inputs (int): number of input features i.e. number of wavelength bins.
+          num_hidden_layers (int): number of hidden (linear) layers.
         """
         super().__init__()
         # Conv1D layer parameters
@@ -150,20 +195,28 @@ class cnn_network(nn.Module):
         self.conv_num_out = int(self.conv_num_out)
 
         # linear layer parameters
+        self.num_hidden_layers = num_hidden_layers
         self.hidden_size = hidden_size
         self.num_inputs = num_inputs
 
-        # define layers
+        # define layers (ordered dict)
+        layers = OrderedDict()
+
+        layers['conv1'] = nn.Conv1d(in_channels=1, out_channels=1, 
+                               kernel_size=self.kernel_size, stride=self.stride, 
+                               padding=self.padding, dilation=self.dilation)
+        layers['conv1_relu'] = nn.ReLU()
+        layers['lin1'] = nn.Linear(self.conv_num_out, self.hidden_size) # hidden layer, fully connected
+        layers['lin1_relu'] = nn.ReLU()
+
+        for i in range(self.num_hidden_layers-1):
+            layers[f'lin{i+2}'] = nn.Linear(self.hidden_size, self.hidden_size)
+            layers[f'lin{i+2}_relu'] = nn.ReLU()
+
+        layers['lin_final'] = nn.Linear(self.hidden_size, 1) # output layer
+        
         # Sequential = ordered container of modules (data passed through each module in order)
-        self.relu_stack = nn.Sequential(
-            nn.Conv1d(in_channels=1, out_channels=1, 
-                      kernel_size=self.kernel_size, stride=self.stride, 
-                      padding=self.padding, dilation=self.dilation),
-            nn.ReLU(), 
-            nn.Linear(self.conv_num_out, self.hidden_size), # hidden layer, fully connected
-            nn.ReLU(),
-            nn.Linear(self.hidden_size, 1), # output layer
-        )
+        self.relu_stack = nn.Sequential(layers)
 
         # diagnostics
         self.total_num_params = sum(p.numel() for p in self.parameters())
@@ -174,7 +227,7 @@ class cnn_network(nn.Module):
         L = L.squeeze(-1) # remove the extra dimension corresponding to the channel number
         return L
 
-def train(model_type='cnn', hidden_size=8, kernel_size=3, verbose=True):
+def train(model_type='cnn', hidden_size=8, kernel_size=3, num_hidden_layers=1, verbose=True):
     """ Train the mockspec model. """
     torch.manual_seed(424242+1)
     rng = np.random.default_rng(424242)
@@ -187,12 +240,16 @@ def train(model_type='cnn', hidden_size=8, kernel_size=3, verbose=True):
     instrument = 'SDSS-BOSS'
     EW_minmax = [3.0, 6.0] # Ang, for testing
 
+    # noise data augmentation
+    SNR = None # signal to noise ratio (None for no noise addition)
+    num_noisy_per_sample = 1
+
     # learning parameters
     test_fraction = 0.2 # fraction of data to reserve for testing
     batch_size = 100 # number of data samples propagated through the network before params are updated
     learning_rate = 1e-3 # i.e. prefactor on grads for gradient descent
-    acc_tol = 0.05 # acceptable tolerance for reporting the prediction accuracy (untransformed space, i.e. EW/ang)
-    epochs = 50 # number of times to iterate over the dataset
+    acc_tol = 0.05 # acceptable tolerance for reporting the prediction accuracy (EW/ang)
+    epochs = 15 # number of times to iterate over the dataset
 
     modelFilename = 'mockspec_model_%s_%s-%s_%d-%d.pth' % (model_type,ion.replace(' ',''),instrument,hidden_size,kernel_size)
 
@@ -203,7 +260,8 @@ def train(model_type='cnn', hidden_size=8, kernel_size=3, verbose=True):
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
     # load data
-    data = MockSpectraDataset(sim, redshift, ion, instrument, model_type, EW_minmax)
+    data = MockSpectraDataset(sim, redshift, ion, instrument, model_type, EW_minmax, 
+                              SNR, num_noisy_per_sample)
 
     spec_n = data[0][0].shape[-1] # number of wavelength bins
     
@@ -232,9 +290,15 @@ def train(model_type='cnn', hidden_size=8, kernel_size=3, verbose=True):
 
     # define model
     if model_type == 'mlp':
-        model = mlp_network(hidden_size=hidden_size, num_inputs=spec_n)
+        model = mlp_network(hidden_size=hidden_size, num_inputs=spec_n,
+                            num_hidden_layers=num_hidden_layers)
     if model_type == 'cnn':
-        model = cnn_network(kernel_size=kernel_size, hidden_size=hidden_size, num_inputs=spec_n)
+        model = cnn_network(kernel_size=kernel_size, hidden_size=hidden_size, 
+                            num_inputs=spec_n, num_hidden_layers=num_hidden_layers)
+
+    print(f'\nModel: [{model_type}] with layers/parameters:')
+    for name, param in model.named_parameters():
+        print(f' [{name}] {param.shape}')
 
     # define loss function
     loss_f = nn.MSELoss() # mean square error
