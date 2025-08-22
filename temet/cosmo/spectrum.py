Mockspec: bugfix for Voigt normalization at z>0, switch to faddeeva985 algorithm, speed optimizations.

temet/cosmo/spectrum.py
@@ -13,7 +13,7 @@ from numba import jit
 from numba.extending import get_cython_function_address
 import ctypes
 
-from ..util.helper import closest, pSplitRange
+from ..util.helper import closest, pSplitRange, faddeeva985
 from ..util.voronoiRay import rayTrace
 
 # default configuration for ray generation
@@ -42,10 +42,10 @@ lines = {'HI 1215'    : {'f':0.4164,   'gamma':6.26e8,  'wave0':1215.670,  'ion'
          'HI 923'     : {'f':2.216e-3, 'gamma':5.79e6,  'wave0':923.1503,  'ion':'H I'},
          'HI 920'     : {'f':1.605e-3, 'gamma':4.19e6,  'wave0':920.9630,  'ion':'H I'},
          'HI 919'     : {'f':1.20e-3,  'gamma':7.83e4,  'wave0':919.3514,  'ion':'H I'},
-         'HI 918'     : {'f':9.21e-4,  'gamma':5.06e4,  'wave0':918.1293,  'ion':'H I'},
-         'HI 917'     : {'f':7.226e-4, 'gamma':3.39e4,  'wave0':917.1805,  'ion':'H I'},
-         'HI 916'     : {'f':5.77e-4,  'gamma':2.34e4,  'wave0':916.4291,  'ion':'H I'},
-         'HI 915'     : {'f':4.69e-4,  'gamma':1.66e4,  'wave0':915.8238,  'ion':'H I'},
+         #'HI 918'     : {'f':9.21e-4,  'gamma':5.06e4,  'wave0':918.1293,  'ion':'H I'},
+         #'HI 917'     : {'f':7.226e-4, 'gamma':3.39e4,  'wave0':917.1805,  'ion':'H I'},
+         #'HI 916'     : {'f':5.77e-4,  'gamma':2.34e4,  'wave0':916.4291,  'ion':'H I'},
+         #'HI 915'     : {'f':4.69e-4,  'gamma':1.66e4,  'wave0':915.8238,  'ion':'H I'},
          'CI 1561'    : {'f':7.14e-2,  'gamma':1.17e8,  'wave0':1561.054,  'ion':'C I'}, # 6 subcomponents combined
         #'CI 1560a'   : {'f':7.14e-2,  'gamma':6.52e7,  'wave0':1560.309,  'ion':'C I'}, # test: 'a' of above
         #'CI 1560b'   : {'f':5.36e-2,  'gamma':8.81e7,  'wave0':1560.682,  'ion':'C I'}, # test: 'b' of above
@@ -529,7 +529,7 @@ def create_wavelength_grid(line=None, instrument=None):
         num_edges = int(np.floor((wave_max - wave_min) / dwave)) + 1
         wave_edges = np.linspace(wave_min, wave_max, num_edges)
         wave_mid = (wave_edges[1:] + wave_edges[:-1]) / 2
-        print(f'Created [N = {wave_mid.size}] linear wavelength grid with {dwave = } for [{instrument}]')
+        print(f' Created [N = {wave_mid.size}] linear wavelength grid with {dwave = } [{wave_min =} {wave_max =}] for [{instrument}]')
 
     # if dlogwave is specified, use log10-linear wavelength spacing
     if dlogwave is not None:
@@ -539,7 +539,7 @@ def create_wavelength_grid(line=None, instrument=None):
         wave_mid = 10.0**log_wave_mid
         log_wave_edges = np.arange(log_wavemin-dlogwave/2,log_wavemax+dlogwave+dlogwave/2,dlogwave)
         wave_edges = 10.0**log_wave_edges
-        print(f'Created [N = {wave_mid.size}] loglinear wavelength grid with {dlogwave = } for [{instrument}]')
+        print(f' Created [N = {wave_mid.size}] loglinear wavelength grid with {dlogwave = } for [{instrument}]')
 
     # if dlnwave is specified, use log-linear wavelength spacing
     if dlnwave is not None:
@@ -549,7 +549,7 @@ def create_wavelength_grid(line=None, instrument=None):
         wave_mid = np.exp(log_wave_mid)
         log_wave_edges = np.arange(log_wavemin-dlnwave/2,log_wavemax+dlnwave+dlnwave/2,dlnwave)
         wave_edges = np.exp(log_wave_edges)
-        print(f'Created [N = {wave_mid.size}] lnlinear wavelength grid with {dlnwave = } for [{instrument}]')
+        print(f' Created [N = {wave_mid.size}] lnlinear wavelength grid with {dlnwave = } for [{instrument}]')
 
     if wave_mid is None:
         raise Exception(f'Missing wavelength grid specification for [{instrument}].')
@@ -564,7 +564,7 @@ def create_wavelength_grid(line=None, instrument=None):
         wave_mid = np.exp(log_wave_mid)
         log_wave_edges = np.arange(log_wavemin-d_loglam/2,log_wavemax+d_loglam+d_loglam/2,d_loglam)
         wave_edges = np.exp(log_wave_edges)
-        print(f'Created [N = {wave_mid.size}] loglinear wavelength grid with {R = } for [{instrument}]')
+        print(f' Created [N = {wave_mid.size}] loglinear wavelength grid with {R = } for [{instrument}]')
 
     tau_master = np.zeros(wave_mid.size, dtype='float32')
 
@@ -578,18 +578,78 @@ functype = ctypes.CFUNCTYPE(ctypes.c_double, ctypes.c_double, ctypes.c_double)
 # Note: rather dangerous as the real part isn't strictly guaranteed to be the first 8 bytes
 wofz_complex_fn_realpart = functype(addr)
 
-@jit(nopython=True, nogil=True, cache=False)
-def _voigt0(wave_cm, b, wave0_ang, gamma):
-    """ Dimensionless Voigt profile (shape).
+@jit(nopython=True, nogil=True)
+def _voigt_tau(wave, N, b, wave0, f, gamma, wave0_rest=None):
+    """ Compute optical depth tau as a function of wavelength for a Voigt absorption profile.
 
     Args:
-      wave_cm (array[float]): wavelength grid in [cm] where the profile is calculated.
-      b (float): doppler parameter in km/s.
-      wave0_ang (float): central wavelength of transition in angstroms.
-      gamma (float): sum of transition probabilities (Einstein A coefficients).
+      wave (array[float]): observed-frame wavelength grid in [linear ang]
+      N (float): column density of absorbing species in [cm^-2]
+      b (float): doppler parameter, equal to sqrt(2kT/m) where m is the particle mass.
+        b = sigma*sqrt(2) where sigma is the velocity dispersion.
+      wave0 (float): observed-frame central wavelength of the transition in [ang]
+      f (float): oscillator strength of the transition
+      gamma (float): sum of transition probabilities (Einstein A coefficients) [1/s]
+      wave0_rest (float): if not None, then rest-frame central wavelength, i.e. wave0 could be redshifted
     """
+    if wave0_rest is None:
+        wave0_rest = wave0
+
+    wave_cm = wave * 1e-8
+
+    # get dimensionless shape for voigt profile:
     nu = sP_units_c_cgs / wave_cm # wave = c/nu
-    wave_rest = wave0_ang * 1e-8 # angstrom -> cm
+    wave0_cm = wave0 * 1e-8 # angstrom -> cm
+    wave0_rest_cm = wave0_rest * 1e-8 # angstrom -> cm
+    nu0 = sP_units_c_cgs / wave0_cm # Hz
+    b_cgs = b * 1e5 # km/s -> cm/s
+    dnu = b_cgs / wave0_cm # Hz, "doppler width" = sigma/sqrt(2)
+
+    # use Faddeeva for integral
+    #alpha = gamma / (4*np.pi*dnu) # old, wrong for z>0
+    alpha = gamma / (4*np.pi*b_cgs/wave0_rest_cm) # should use here rest-frame wave0
+    voigt_u = (nu - nu0) / dnu # = (nu-nu0) * wave0_cm / b_cgs
+    # = (c/wave_cm - c/wave0_cm) * wave0_cm / b_cgs
+    # = c * (wave0_cm/wave_cm - 1) / b_cgs
+
+    # numba wofz issue: https://github.com/numba/numba/issues/3086
+    #voigt_wofz = wofz(voigt_u + 1j*alpha).real # H(alpha,z)
+    voigt_wofz = np.zeros(voigt_u.size, dtype=np.float64)
+    for i in range(voigt_u.size):
+        #voigt_wofz[i] = wofz_complex_fn_realpart(voigt_u[i], alpha)
+        voigt_wofz[i] = faddeeva985(voigt_u[i], alpha) # speed-up depends on region
+
+    phi_wave = voigt_wofz / b_cgs # s/cm
+
+    # normalize amplitude
+    consts = 0.014971475 # sqrt(pi)*e^2 / m_e / c = cm^2/s
+    wave0_rest_cm = wave0_rest * 1e-8
+
+    tau_wave = (consts * N * f * wave0_rest_cm) * phi_wave # dimensionless
+    return tau_wave
+
+#@jit(nopython=True, nogil=True, cache=False)
+def _voigt_tau_old(wave, N, b, wave0, f, gamma, wave0_rest=None):
+    """ Compute optical depth tau as a function of wavelength for a Voigt absorption profile.
+
+    Args:
+      wave (array[float]): wavelength grid in [linear ang]
+      N (float): column density of absorbing species in [cm^-2]
+      b (float): doppler parameter, equal to sqrt(2kT/m) where m is the particle mass.
+        b = sigma*sqrt(2) where sigma is the velocity dispersion.
+      wave0 (float): central wavelength of the transition in [ang]
+      f (float): oscillator strength of the transition
+      gamma (float): sum of transition probabilities (Einstein A coefficients) [1/s]
+      wave0_rest (float): if not None, then rest-frame central wavelength, i.e. wave0 could be redshifted
+    """
+    if wave0_rest is None:
+        wave0_rest = wave0
+
+    wave_cm = wave * 1e-8
+
+    # get dimensionless shape for voigt profile:
+    nu = sP_units_c_cgs / wave_cm # wave = c/nu
+    wave_rest = wave0 * 1e-8 # angstrom -> cm
     nu0 = sP_units_c_cgs / wave_rest # Hz
     b_cgs = b * 1e5 # km/s -> cm/s
     dnu = b_cgs / wave_rest # Hz, "doppler width" = sigma/sqrt(2)
@@ -607,30 +667,8 @@ def _voigt0(wave_cm, b, wave0_ang, gamma):
         voigt_wofz[i] = wofz_complex_fn_realpart(voigt_u[i], alpha)
 
     phi_wave = voigt_wofz / b_cgs # s/cm
-    return phi_wave
 
-@jit(nopython=True, nogil=True, cache=False)
-def _voigt_tau(wave, N, b, wave0, f, gamma, wave0_rest=None):
-    """ Compute optical depth tau as a function of wavelength for a Voigt absorption profile.
-
-    Args:
-      wave (array[float]): wavelength grid in [linear ang]
-      N (float): column density of absorbing species in [cm^-2]
-      b (float): doppler parameter, equal to sqrt(2kT/m) where m is the particle mass.
-        b = sigma*sqrt(2) where sigma is the velocity dispersion.
-      wave0 (float): central wavelength of the transition in [ang]
-      f (float): oscillator strength of the transition
-      gamma (float): sum of transition probabilities (Einstein A coefficients) [1/s]
-      wave0_rest (float): if not None, then rest-frame central wavelength, i.e. wave0 could be redshifted
-    """
-    if wave0_rest is None:
-        wave0_rest = wave0
-
-    # get dimensionless shape for voigt profile:
-    wave_cm = wave * 1e-8
-
-    phi_wave = _voigt0(wave_cm, b, wave0, gamma)
-
+    # normalize amplitude
     consts = 0.014971475 # sqrt(pi)*e^2 / m_e / c = cm^2/s
     wave0_rest_cm = wave0_rest * 1e-8
 
@@ -689,7 +727,7 @@ def varconvolve(arr, kernel):
 
     return arr_conv
 
-@jit(nopython=True, nogil=True, cache=False)
+@jit(nopython=True, nogil=True)
 def deposit_single_line(wave_edges_master, tau_master, f, gamma, wave0, N, b, z_eff, debug=False):
     """ Add the absorption profile of a single transition, from a single cell, to a spectrum.
     Global method, where the original master grid is assumed to be very high resolution, such that 
@@ -794,7 +832,7 @@ def deposit_single_line(wave_edges_master, tau_master, f, gamma, wave0, N, b, z_
 
     return
 
-@jit(nopython=True, nogil=True, cache=False)
+@jit(nopython=True, nogil=True)
 def _resample_spectrum(master_mid, tau_master, inst_waveedges):
     """ Resample a high-resolution spectrum defined on the master_mid wavelength (midpoints) grid, 
     with given optical depths at each wavelength point, onto a lower resolution inst_waveedges 
@@ -817,9 +855,6 @@ def _resample_spectrum(master_mid, tau_master, inst_waveedges):
 
     assert master_startind > 0, 'Should not occur.'
     assert master_finalind < master_mid.size-1, 'Should not occur.'
-
-    # convert optical depth to flux
-    flux_master = np.exp(-tau_master)
 
     dwave_master = master_mid[1] - master_mid[0] # constant
 
@@ -863,11 +898,11 @@ def _resample_spectrum(master_mid, tau_master, inst_waveedges):
             flux_bin = 0.0
 
         # accumulate (partial) sum of 1-flux
-        flux_bin += (1-flux_master[master_ind])
+        flux_bin += 1 - np.exp(-tau_master[master_ind])
 
     return inst_tau
 
-@jit(nopython=True, nogil=True, cache=False)
+@jit(nopython=True, nogil=True)
 def _create_spectra_from_traced_rays(f, gamma, wave0, ion_mass, 
                                      rays_off, rays_len, rays_cell_dl, rays_cell_inds, 
                                      cell_dens, cell_temp, cell_vellos, z_vals, z_lengths,
@@ -895,6 +930,13 @@ def _create_spectra_from_traced_rays(f, gamma, wave0, ion_mass,
         master_temp = cell_temp[master_inds]
         master_vellos = cell_vellos[master_inds]
 
+        # column density
+        N = master_dens * (master_dx * sP_units_Mpc_in_cm) # cm^-2
+
+        # skip rays with negligibly small total columns (in linear cm^-2)
+        if np.sum(N) < 1e8:
+            continue
+
         # reset tau_master for each ray
         tau_master *= 0.0
 
@@ -912,22 +954,24 @@ def _create_spectra_from_traced_rays(f, gamma, wave0, ion_mass,
         # effective redshift
         z_eff = (1+z_doppler)*(1+z_cosmo) - 1
 
-        # column density
-        N = master_dens * (master_dx * sP_units_Mpc_in_cm) # cm^-2
-
         # doppler parameter b = sqrt(2kT/m) where m is the particle mass
         b = np.sqrt(2 * sP_units_boltzmann * master_temp / ion_mass) # cm/s
         b /= 1e5 # km/s
 
         # deposit each intersected cell as an absorption profile onto spectrum
+        count = 0
         for j in range(length):
             # skip negligibly small columns (in linear cm^-2) for efficiency
-            if N[j] < 1e2:
+            if N[j] < 1e6:
                 continue
 
             deposit_single_line(master_edges, tau_master, f, gamma, wave0, N[j], b[j], z_eff[j])
+            count += 1
 
         # resample tau_master on to instrument wavelength grid
+        if count == 0:
+            continue # no absorption, skip this ray
+
         tau_inst = _resample_spectrum(master_mid, tau_master, inst_waveedges)
 
         # line spread function (LSF) in pixel space? convolve the instrumental (flux) spectrum now
@@ -936,19 +980,25 @@ def _create_spectra_from_traced_rays(f, gamma, wave0, ion_mass,
         if lsf_mode == 1:
             flux_inst = 1 - np.exp(-tau_inst)
             flux_conv = varconvolve(flux_inst, lsf_matrix)
-            #tau_inst = varconvolve(tau_inst, lsf_matrix) # old, remove
+
+            # note: flux_conv can be 1.0, leading to tau_inst = inf
+            # so set to 1-eps, such that tau is very large (~16 for this value of eps)
+            # note: taking 1 - 1e-10 as in _resample_spectrum() fails in where/assignment to flux_conv (dtype?)
+            flux_smallval = 1.0 - 1e-7
+            flux_conv[flux_conv >= 1.0] = flux_smallval
+
             tau_inst = -np.log(1-flux_conv)
 
-        # also compute and save a reference EW from master
+        # also compute and save a reference EW
         # note: is a global EW, i.e. not localized/restricted to a single absorber
-        EW_allrays[i] = _equiv_width(tau_master,master_mid)
+        EW_allrays[i] = _equiv_width(tau_inst,inst_wavemid)
 
         # stamp
         tau_allrays[i,:] = tau_inst
 
         # debug: (verify EW is same in master and instrumental grids)
-        if 1:
-            EW_check = _equiv_width(tau_inst,inst_wavemid)
+        if 0:
+            EW_check = _equiv_width(tau_master,master_mid)
             #assert np.abs(EW_check - EW_allrays[i]) < 0.01
             if np.abs(EW_check - EW_allrays[i]) > 0.01:
                 # where? ignore if it is in master grid outside of inst grid coverage
@@ -995,10 +1045,6 @@ def create_spectra_from_traced_rays(sP, line, instrument,
 
     z_lengths = sP.units.redshiftToComovingDist(z_vals) - sP.units.redshiftToComovingDist(sP.redshift)
 
-    # adapt master grid to span instrumental grid (optional, save some memory/efficiency)
-    instruments['master']['wave_min'] = instruments[instrument]['wave_min'] - 100
-    instruments['master']['wave_max'] = instruments[instrument]['wave_max'] + 100
-
     # sample master, and instrumental, grids
     master_mid, master_edges, _ = create_wavelength_grid(instrument='master')
 
@@ -1008,6 +1054,15 @@ def create_spectra_from_traced_rays(sP, line, instrument,
     assert inst_waveedges[-1] <= master_edges[-1], 'Instrumental wavelength grid max extends off master.'
 
     lsf_mode, lsf, _ = lsf_matrix(instrument)
+
+    if 0:
+        if sP.simName == 'TNG50-1': indiv_index = 9420
+        if sP.simName == 'TNG50-3': indiv_index = 58189
+
+        rays_len = rays_len[indiv_index:indiv_index+10]
+        rays_off = rays_off[indiv_index:indiv_index+10]
+        n_rays = rays_len.size
+        print('TODO REMOVE SINGLE RAY DEBUG!!!')
 
     # single-threaded
     if nThreads == 1 or n_rays < nThreads:
@@ -1455,6 +1510,8 @@ def _integrate_quantity_along_traced_rays(rays_off, rays_len, rays_cell_dl, rays
 
         r[i] = np.sum(master_dx * master_values)
 
+        #import pdb; pdb.set_trace()
+
     return r
 
 def integrate_along_saved_rays(sP, field, nRaysPerDim=nRaysPerDim_def, raysType=raysType_def, subhaloIDs=None, pSplit=None):
@@ -1617,6 +1674,25 @@ def generate_spectra_from_saved_rays(sP, ion='Si II', instrument='4MOST-HRS', nR
       solar (bool): if True, do not use simulation-tracked metal abundances, but instead 
         use the (constant) solar value.
     """
+    # adapt idealized grid to span (redshifted) central wavelength (optional, save space)
+    if instrument == 'idealized':
+        wave_min_ion = np.inf
+        wave_max_ion = 0.0
+
+        for line_name, props in lines.items():
+            if props['ion'] == ion:
+                wave_min_ion = min(wave_min_ion, props['wave0'])
+                wave_max_ion = max(wave_max_ion, props['wave0'])
+
+        wave_min = np.floor((wave_min_ion * (1 + sP.redshift) - 100) / 500) * 500
+        wave_max = np.ceil((wave_max_ion * (1 + sP.redshift) + 100) / 500) * 500
+        instruments['idealized']['wave_min'] = wave_min
+        instruments['idealized']['wave_max'] = wave_max
+
+    # adapt master grid to span instrumental grid (optional, save some memory/efficiency)
+    instruments['master']['wave_min'] = instruments[instrument]['wave_min'] - 100
+    instruments['master']['wave_max'] = instruments[instrument]['wave_max'] + 100
+
     # sample master grid
     wave_mid, _, tau = create_wavelength_grid(instrument=instrument)
     

temet/plot/spectrum.py
@@ -18,7 +18,7 @@ from ..util.helper import sampleColorTable, logZeroNaN
 from ..util.units import units
 from ..plot.config import *
 
-def _cog(lines, N, b, nPts=401):
+def _cog(lines, N, b, nPts=1001):
     """ Compute a local absorption spectrum for one or more line(s) at a given column density and 
     Doppler parameter, returning (relative) flux array and derived equivalent width. 
     
@@ -31,7 +31,7 @@ def _cog(lines, N, b, nPts=401):
     """
     f, gamma, wave0_ang, _, _ = _line_params(lines[0])
 
-    wave_ang = np.linspace(wave0_ang-10, wave0_ang+10, nPts) # 0.05 Ang spacing
+    wave_ang = np.linspace(wave0_ang-20, wave0_ang+20, nPts) # 0.05 Ang spacing
     dvel = (wave_ang/wave0_ang - 1) * units.c_cgs / 1e5 # cm/s -> km/s
 
     #dwave_ang = wave_ang[1] - wave_ang[0]
@@ -42,6 +42,9 @@ def _cog(lines, N, b, nPts=401):
     for line in lines:
         f, gamma, wave0_ang, _, _ = _line_params(line)
         tau += _voigt_tau(wave_ang, 10.0**N, b, wave0_ang, f, gamma)
+
+    if tau[0] > 1e-4 or tau[-1] > 1e-4:
+        print('_cog(): Warning: optical depth at edges is large, consider increasing wave_ang range.')
 
     flux = np.exp(-1*tau)
     EW = _equiv_width(tau,wave_ang)
@@ -933,7 +936,7 @@ def EW_distribution(sim_in, line='MgII 2796', instrument='SDSS-BOSS', redshifts=
         EWs[redshift] = data
 
     # start plot
-    fig = plt.figure(figsize=figsize)
+    fig = plt.figure(figsize=[figsize[0]*0.85, figsize[1]*0.85])
     ax = fig.add_subplot(111)
 
     ax.set_xlim(xlim)
@@ -980,16 +983,26 @@ def EW_distribution(sim_in, line='MgII 2796', instrument='SDSS-BOSS', redshifts=
         opts = {'color':'#333333', 'ecolor':'#333333', 'alpha':0.6, 'capsize':0.0, 'fmt':'s'}
         ax.errorbar(m13_x, m13_y, yerr=m13_yerr, xerr=xerr, label=m13_label, **opts)
 
-        # Chen+16 (updated/finished sample of Matejek+, identical EW bins)
+        # Chen+17 (updated/finished sample of Matejek+, identical EW bins)
         # https://ui.adsabs.harvard.edu/abs/2017ApJ...850..188C/abstract
         c16_y = [1.539, 0.591, 0.298, 0.185, 0.134, 0.026]
         c16_yerr = [0.215, 0.082, 0.055, 0.042, 0.035, 0.007]
-        c16_label = 'Chen+16 (1.9 < z < 6.3)'
+        c16_label = 'Chen+17 (1.9 < z < 6.3)'
 
         opts = {'color':'#333333', 'ecolor':'#333333', 'alpha':0.9, 'capsize':0.0, 'fmt':'D'}
         ax.errorbar(m13_x, c16_y, yerr=c16_yerr, xerr=xerr, label=c16_label, **opts)
 
         # check: https://iopscience.iop.org/article/10.3847/1538-4357/abbb34/pdf
+
+        # Sebastian+24 (E-XQR-30)
+        s24_x = [5.15e-2, 1.03e-1, 2.0e-1, 3.95e-1, 7.75e-1, 1.51e0, 2.94e0]
+        s24_y = [5.51e0, 4.12e0, 3.13e0, 1.55e0, 6.1e-1, 2.27e-1, 8.10e-2]
+        s24_y0 = [4.48e0, 3.37e0, 2.80e0, 1.38e0, 5.17e-1, 1.86e-1, 6.43e-2]
+        s24_yerr = np.array(s24_y) - np.array(s24_y0)
+        s24_label = 'Sebastian+24 (2 < z < 6)'
+
+        opts = {'color':'#000', 'ecolor':'#000', 'alpha':0.9, 'capsize':0.0, 'fmt':'o'}
+        ax.errorbar(s24_x, s24_y, yerr=s24_yerr, label=s24_label, **opts)
 
     if line == 'CIV 1548':
         # Finlator+
@@ -1002,7 +1015,7 @@ def EW_distribution(sim_in, line='MgII 2796', instrument='SDSS-BOSS', redshifts=
     plt.close(fig)
 
 def EW_vs_coldens(sim, line='CIV 1548', instrument='SDSS-BOSS', bvals = [5, 10, 25, 50, 100, 150],
-                    xlim=[12.5,16.0], ylim=[-1.7,0.5], solar=False, log=False):
+                    xlim=[12.5,16.0], ylim=[-1.7,0.5], solar=False, log=False, pSplit=None):
     """ Plot the relationship between EW and column density, and compare to the CoG.
 
     Args:
@@ -1014,19 +1027,16 @@ def EW_vs_coldens(sim, line='CIV 1548', instrument='SDSS-BOSS', bvals = [5, 10, 
       ylim (list[float]): min and max equivalent width [log Ang].
       solar (bool): use the (constant) solar value instead of simulation-tracked metal abundances.
       log (bool): plot log(EW) instead of linear EWs.
+      pSplit (list[int]): if not None, then use this subset.
     """
-    # plot config
-    EW_min = 1e-2 # rest-frame ang
-    coldens_min = 10.0 # log 1/cm^2
-
     # load
     ion = lines[line]['ion']
-    filepath = _spectra_filepath(sim, ion, instrument=instrument, solar=solar)
+    filepath = _spectra_filepath(sim, ion, instrument=instrument, solar=solar, pSplit=pSplit)
 
     with h5py.File(filepath,'r') as f:
         EW = f['EW_%s' % line.replace(' ','_')][()]
 
-    coldens = integrate_along_saved_rays(sim, '%s numdens' % ion)
+    coldens = integrate_along_saved_rays(sim, '%s numdens' % ion, pSplit=pSplit)
 
     # convert to rest-frame
     EW /= (1+sim.redshift)
@@ -1036,7 +1046,7 @@ def EW_vs_coldens(sim, line='CIV 1548', instrument='SDSS-BOSS', bvals = [5, 10, 
         EW = np.log10(EW)
         coldens = np.log10(coldens)
 
-        w = np.where( (EW>ylim[0]) & (EW<=ylim[1]) & (coldens>xlim[0]) & (coldens<=xlim[1]))
+        w = np.where((EW>ylim[0]) & (EW<=ylim[1]) & (coldens>xlim[0]) & (coldens<=xlim[1]))
 
     print('[%s] %d/%d sightlines selected.' % (sim.simName,len(w[0]),len(EW)))
 
@@ -1068,7 +1078,6 @@ def EW_vs_coldens(sim, line='CIV 1548', instrument='SDSS-BOSS', bvals = [5, 10, 
     # overplot curve of growth
     Nvals = np.linspace(xlim[0], xlim[1], 100)
     
-
     for b in bvals:
         EWs = np.zeros(Nvals.size, dtype='float32')
         for i, N in enumerate(Nvals):
@@ -1100,11 +1109,16 @@ def dNdz_evolution(sim_in, redshifts, line='MgII 2796', instrument='SDSS-BOSS', 
     # config
     z13 = zhu13mgii()
     #EW_thresholds = [0.3,1.0,3.0] # thresholds for EW for vs. redshift plot
-    EW_thresholds = z13['EW0'] # match to obs data
+    if 'MgII' in line:
+        EW_thresholds = z13['EW0'] # match to obs data
+        xlim = [0.0, np.max(redshifts)+2.0]
+        ylim = [2e-4, 4.0]
 
-    xlim = [0.0, np.max(redshifts)+1]
-    ylim = [2e-4, 4.0]
-
+    if 'CIV' in line:
+        EW_thresholds = [0.005, 0.3, 0.6, 0.9]
+        xlim = [np.min(redshifts)-0.1, np.max(redshifts)+0.1]
+        ylim = [1e-3, 10.0]    
+    
     # load: loop over all available redshifts
     zz = []
     dNdz = {thresh:[] for thresh in EW_thresholds}
@@ -1142,7 +1156,7 @@ def dNdz_evolution(sim_in, redshifts, line='MgII 2796', instrument='SDSS-BOSS', 
         zz.append(redshift)
 
     # start plot
-    fig = plt.figure(figsize=figsize)
+    fig = plt.figure(figsize=[figsize[0]*0.85, figsize[1]*0.85])
     ax = fig.add_subplot(111)
 
     ax.set_xlim(xlim)
@@ -1154,7 +1168,8 @@ def dNdz_evolution(sim_in, redshifts, line='MgII 2796', instrument='SDSS-BOSS', 
     # plot the simulation dN/dz for each EW threshold
     colors = []
     for EW_thresh in EW_thresholds:
-        l, = ax.plot(zz, dNdz[EW_thresh], '-', lw=lw, label=r'EW > %.1f$\,\rm{\AA}$' % EW_thresh)
+        label = r'%s (EW > %.1f$\,\rm{\AA}$)' % (sim.simName,EW_thresh)
+        l, = ax.plot(zz, dNdz[EW_thresh], '-', lw=lw, label=label)
         colors.append(l.get_color())
 
     # observational data
@@ -1178,7 +1193,7 @@ def dNdz_evolution(sim_in, redshifts, line='MgII 2796', instrument='SDSS-BOSS', 
     plt.close(fig)
 
     # start plot
-    fig = plt.figure(figsize=figsize)
+    fig = plt.figure(figsize=[figsize[0]*0.85, figsize[1]*0.85])
     ax = fig.add_subplot(111)
 
     ax.set_xlim(xlim)
@@ -1188,7 +1203,7 @@ def dNdz_evolution(sim_in, redshifts, line='MgII 2796', instrument='SDSS-BOSS', 
     ax.set_yscale('log')
 
     for EW_thresh in EW_thresholds:
-        ax.plot(zz, dNdX[EW_thresh], '-', lw=lw, label=r'EW > %.1f$\,\rm{\AA}$' % EW_thresh)
+        ax.plot(zz, dNdX[EW_thresh], '-', lw=lw)
 
     # observational data
     if line == 'MgII 2796':
@@ -1196,12 +1211,57 @@ def dNdz_evolution(sim_in, redshifts, line='MgII 2796', instrument='SDSS-BOSS', 
         # Zou+21
         # https://ui.adsabs.harvard.edu/abs/2017MNRAS.472.1023C/abstract
 
-    if line == 'CIV': # also SiIV
-        pass # https://ui.adsabs.harvard.edu/abs/2022ApJ...924...12H/abstract
+    if line == 'CIV 1548':
+        # Hasan+2020 (Table 4, Figure 7)
+        h20_label = 'Hasan+ (2020)'
+        h20_z = [1.34, 1.74, 1.94, 2.13, 2.30, 2.46, 2.64, 2.83, 3.01, 4.0]
+        h20_dNdX_005A = [1.97, 2.17, 2.12, 2.11, 2.03, 2.02, 1.77, 2.13, 1.39, 1.23]
+        h20_dNdX_03A  = [0.80, 0.76, 0.75, 0.67, 0.61, 0.56, 0.59, 0.55, 0.40, 0.24]
+        h20_dNdX_06A  = [0.53, 0.39, 0.36, 0.34, 0.34, 0.19, 0.26, 0.20, 0.11, 0.06]
+        h20_dNdX_005A_err = [0.19, 0.21, 0.20, 0.19, 0.18, 0.18, 0.16, 0.19, 0.12, 0.11]
+        h20_dNdX_03A_err  = [0.10, 0.10, 0.10, 0.10, 0.09, 0.09, 0.09, 0.09, 0.06, 0.04]
+        h20_dNdX_06A_err  = [0.08, 0.07, 0.07, 0.07, 0.07, 0.05, 0.06, 0.06, 0.03, 0.02]
+
+        label = r'%s ($\rm{W_0 > 0.05 \AA}$)' % (h20_label)
+        ax.errorbar(h20_z, h20_dNdX_005A, yerr=h20_dNdX_005A_err, color=colors[0], marker='s', ls='none', label=label)
+        label = r'%s ($\rm{W_0 > 0.3 \AA}$)' % (h20_label)
+        ax.errorbar(h20_z, h20_dNdX_03A, yerr=h20_dNdX_03A_err, color=colors[1], marker='s', ls='none', label=label)
+        label = r'%s ($\rm{W_0 > 0.6 \AA}$)' % (h20_label)
+        ax.errorbar(h20_z, h20_dNdX_06A, yerr=h20_dNdX_06A_err, color=colors[2], marker='s', ls='none', label=label)
+
+        # Anand+2025 (Figure 7)
+        a25_label = 'Anand+ (2025)'
+        a25_z = [1.54, 1.77, 1.86, 1.93, 1.98, 2.03, 2.09, 2.14, 2.20, 2.26, 2.33, 2.41, 2.51, 2.66, 2.88, 3.85]
+        a25_dNdX_06A = [1.80e-1, 1.72e-1, 1.75e-1, 1.63e-1, 1.59e-1, 1.52e-1, 1.50e-1, 1.47e-1, 1.41e-1, 
+                        1.33e-1, 1.25e-1, 1.22e-1, 1.07e-1, 8.47e-2, 7.67e-2, 4.81e-2]
+        a25_dNdX_09A = [7.79e-2, 7.07e-2, 6.81e-2, 6.68e-2, 6.60e-2, 6.14e-2, 5.72e-2, 5.86e-2, 5.45e-2, 
+                        5.34e-2, 4.83e-2, 4.54e-2, 4.12e-2, 3.21e-2, 2.80e-2, 1.71e-2]
+
+        label = r'%s ($\rm{W_0 > 0.6 \AA}$)' % (a25_label)
+        ax.errorbar(a25_z, a25_dNdX_06A, color=colors[2], marker='o', ls='none', label=label)
+        label = r'%s ($\rm{W_0 > 0.9 \AA}$)' % (a25_label)
+        ax.errorbar(a25_z, a25_dNdX_09A, color=colors[3], marker='o', ls='none', label=label)
+
+        # https://ui.adsabs.harvard.edu/abs/2022ApJ...924...12H/abstract
         # https://ui.adsabs.harvard.edu/abs/2018MNRAS.481.4940C/abstract
 
+    # legends
+    handles = []
+    labels = []
+
+    for i, EW_thresh in enumerate(EW_thresholds):
+        label = r'%s (EW > %.1f$\,\rm{\AA}$)' % (sim.simName,EW_thresh)
+        if EW_thresh < 0.1:
+            label = r'%s (EW > %.3f$\,\rm{\AA}$)' % (sim.simName,EW_thresh)
+        handles.append(plt.Line2D((0,1), (0,0), color=colors[i], lw=lw, ls='-'))
+        labels.append(label)
+
+    legend2 = ax.legend(handles, labels, loc='upper right')
+    ax.add_artist(legend2)
+
+    ax.legend(loc='lower left')
+
     # finish plot
-    ax.legend(loc='best')
     fig.savefig('dNdX_evolution_%s_%s.pdf' % (sim.simName,line.replace(' ','-')))
     plt.close(fig)
 

temet/test.py
@@ -15,6 +15,63 @@ from .util.helper import pSplitRange
 from illustris_python.util import partTypeNum
 from matplotlib.backends.backend_pdf import PdfPages
 
+def plot_wofz():
+    """ Test wofz complex function. """
+    from .cosmo.spectrum import wofz_complex_fn_realpart
+    from .util.helper import faddeeva985
+
+    u = np.linspace(-15, 15, 200)
+
+    # plot
+    fig, ax = plt.subplots(figsize=figsize)
+
+    for alpha in [0.00001, 0.0001, 0.001, 0.01, 0.1, 0.5, 1.0]:
+        w = np.zeros(u.size, dtype='float32')
+        w2 = np.zeros(u.size, dtype='float32')
+        for i in range(u.size):
+            w[i] = wofz_complex_fn_realpart(u[i], alpha)
+            w2[i] = faddeeva985(u[i], alpha)
+            
+        l, = ax.plot(u, w, '--', lw=2, label='alpha = %.2f' % alpha)
+        ax.plot(u, w2, ':', lw=2, color=l.get_color(), label='alpha = %.2f (985)' % alpha)
+        err = np.max(np.abs(w-w2))
+        print(alpha, w[0], w2[0], err)
+
+    ax.set_xlabel('u')
+    ax.set_ylabel('wofz(u)')
+    ax.set_title('wofz Complex Function Real Part')
+    ax.legend(loc='upper right')
+
+    fig.savefig('wofz_test.pdf')
+    plt.close(fig)
+
+    # plot B
+    fig, ax = plt.subplots(figsize=figsize)
+
+    alpha = np.linspace(-4.0, 0.0, 200)
+    alpha = 10.0**alpha
+
+    for u in [-2.0,-1.5,-1.0,-0.5,-0.1,0.0,0.1,0.5,1.0,1.5,2.0]:
+        w = np.zeros(alpha.size, dtype='float32')
+        w2 = np.zeros(alpha.size, dtype='float32')
+        for i in range(alpha.size):
+            w[i] = wofz_complex_fn_realpart(u, alpha[i])
+            w2[i] = faddeeva985(u, alpha[i])
+
+        l, = ax.plot(np.log10(alpha), w, lw=2, label='u = %.2f' % u)
+        ax.plot(np.log10(alpha), w2, ':', lw=2, color=ax.lines[-1].get_color(), label='u = %.2f (985)' % u)
+
+        err = np.max(np.abs(w-w2))
+        print(u, w[0], w2[0], err)
+
+    #ax.set_xscale('log')
+    ax.set_xlabel('log alpha')
+    ax.set_ylabel('wofz(u)')
+    ax.set_title('wofz Complex Function Real Part')
+    ax.legend(loc='upper right')
+    fig.savefig('wofz_test2.pdf')
+    plt.close(fig)
+
 def concat_tracer_parent_cats():
     """ Combine individual tracer_parent_indextype catalogs into single file. """
     sim = simParams('tng50-1')
@@ -1174,7 +1231,7 @@ def francesca_voversigma():
     # plot
     fig = plt.figure(figsize=[12, 8])
     ax = fig.add_subplot(111)
-    ax.set_xlabel('H-alpha based V$_{\\rm rot}$ / $\sigma$')
+    ax.set_xlabel(r'H-alpha based V$_{\rm rot}$ / $\sigma$')
     ax.set_ylabel('Number of Galaxies')
 
     nBins = 13
@@ -1774,8 +1831,8 @@ def guinevere_mw_sample():
 
         fig = plt.figure(figsize=figsize)
         ax = fig.add_subplot(1,1,1)
-        ax.set_xlabel('log $M_{\\rm gas} / M_\star$ [ Illustris ]')
-        ax.set_ylabel('log $M_{\\rm gas} / M_\star$ [ TNG ]')
+        ax.set_xlabel(r'log $M_{\rm gas} / M_\star$ [ Illustris ]')
+        ax.set_ylabel(r'log $M_{\rm gas} / M_\star$ [ TNG ]')
 
         ax.set_xlim([-2.2, 0.3])
         ax.set_ylim([-2.2, 0.3])